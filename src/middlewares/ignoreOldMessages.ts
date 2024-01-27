01f1c7e774c8ffed558e02da29fac6b1cf1a4ba9
Vlad Pavlenko <pavlenkoliar@gmail.com>
2024-01-27 17:17:14 +0200

    add ignoreOldMessages middleware

---
+++ src/middlewares/ignoreOldMessages.ts
@@ -0,0 +1,15 @@
+import type { Context, NextFunction } from 'grammy';
+
+const afterSeconds = 60;
+
+function dateInSeconds() {
+  return Math.round(Date.now() / 1000);
+}
+
+export async function ignoreOldMessages(ctx: Context, next: NextFunction) {
+  if (!ctx.msg) return await next();
+
+  if (dateInSeconds() - ctx.msg.date < afterSeconds) {
+    return await next();
+  }
+}
