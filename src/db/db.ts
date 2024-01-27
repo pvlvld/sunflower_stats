cf27608cf11735d449efd6cb4196185f4e55c4d9
Vlad Pavlenko <pavlenkoliar@gmail.com>
2024-01-27 17:43:30 +0200

    init db module

---
+++ src/db/db.ts
@@ -0,0 +1,26 @@
+// const mysql = require("mysql");
+import mysql from "mysql";
+
+if (
+  !process.env.DB_HOST ||
+  !process.env.DB_USER ||
+  !process.env.DB_PASSWORD ||
+  !process.env.DB_DATABASE ||
+  !process.env.DB_CHARSET
+) {
+  throw new Error("Provide database env credentials.");
+}
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  charset: process.env.DB_CHARSET,
+});
+
+db.connect((err) => {
+  if (err) throw err;
+});
+
+export default db;
