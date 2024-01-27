bf83c492286dfdde773d15a04c483c482e16b023
Vlad Pavlenko <pavlenkoliar@gmail.com>
2024-01-27 15:31:32 +0200

    russia is a terrorist state

---
+++ src/bot.ts
@@ -0,0 +1,55 @@
+import { Bot, matchFilter, session } from 'grammy';
+import { autoRetry } from '@grammyjs/auto-retry';
+
+
+if (!process.env.BOT_TOKEN) throw new Error('Token required');
+
+const bot = new Bot<MyContext>(process.env.BOT_TOKEN);
+
+const autoRetryTransformer = autoRetry({
+  maxDelaySeconds: 60,
+  maxRetryAttempts: 3,
+  retryOnInternalServerErrors: false,
+});
+
+// PLUGINS
+// Install the session plugin.
+bot.use(
+  session({
+    initial() {
+      return {};
+    },
+  })
+);
+
+// API TRANSFORMERS
+
+bot.api.config.use(async (prev, method, payload, signal) => {
+  if (
+    [
+      'getChat',
+      'getChatMemberCount',
+      'deleteMessage',
+      'answerCallbackQuery',
+    ].includes(method)
+  ) {
+    return autoRetryTransformer(prev, method, payload, signal);
+  }
+
+  return prev(method, payload, signal);
+});
+
+bot.drop(matchFilter('message:is_automatic_forward'));
+
+// MIDDLEWARES
+
+bot.use(ignoreOldMessages);
+bot.use(addFullNameField);
+bot.use(autoThread());
+
+// COMMANDS
+
+const pm = bot.chatType('private');
+const gm = bot.chatType(['group', 'supergroup']);
+
+export { bot };
---
+++ src/index.ts
@@ -0,0 +1,100 @@
+import { bot } from './bot';
+import { GrammyError, webhookCallback, HttpError } from 'grammy';
+import * as http from 'http';
+import process from 'node:process';
+// import { getPublicIP } from './utils/getPublicIP';
+// import { startStatsCollecting } from './utils/statsCollector';
+
+process.on('uncaughtException', function (err) {
+  console.error(err);
+  console.log('Node NOT Exiting...');
+});
+
+function getDate() {
+  return new Date().toLocaleString('uk', {
+    timeZone: 'Europe/Kyiv',
+  });
+}
+
+bot.catch((err) => {
+  const ctx = err.ctx;
+  console.error(`Error while handling update ${ctx.update.update_id}:`);
+  const e = err.error;
+  if (e instanceof GrammyError) {
+    console.error('Error in request:', e.description);
+  } else if (e instanceof HttpError) {
+    console.error('Could not contact Telegram:', e);
+  } else {
+    console.error('Unknown error:', e);
+  }
+});
+
+let server: http.Server = {} as http.Server;
+
+if (process.env.NODE_ENV === 'production') {
+  const app = http.createServer(webhookCallback(bot, 'http'));
+
+  server = app.listen(Number(process.env.PORT ?? 8443));
+
+  bot.api.deleteWebhook({ drop_pending_updates: true }).then(async () => {
+    await bot.api
+      .setWebhook(
+        `https://${await getPublicIP()}/${String(process.env.BOT_TOKEN)}`
+      )
+      .catch((e) => {
+        throw new Error(e);
+      });
+  });
+} else if (process.env.NODE_ENV === 'test') {
+  const app = http.createServer(webhookCallback(bot, 'http'));
+  server = app.listen(Number(process.env.PORT ?? 8443));
+
+  console.log('Started in test mode');
+} else {
+  bot.api.deleteWebhook({ drop_pending_updates: true }).then(() => {
+    bot.start({
+      drop_pending_updates: true,
+      allowed_updates: [
+        'message',
+        'my_chat_member',
+        'chat_member',
+        'callback_query',
+      ],
+    });
+
+    // startStatsCollecting();
+    console.log('Bot is started.');
+  });
+}
+
+process.on('SIGINT', async () => await shutdown());
+process.on('SIGTERM', async () => await shutdown());
+
+let shutdownCounter = 0;
+
+async function shutdown() {
+  if (shutdownCounter) return;
+  shutdownCounter++;
+
+  console.log('Shutting down.');
+  await bot.api.deleteWebhook().then(() => {
+    console.log('Webhook removed');
+  });
+
+  await bot.stop().then(() => {
+    console.log('- Bot stopped.');
+  });
+
+  if ('close' in server) {
+    server.close(() => {
+      console.log('- Server closed.');
+    });
+  }
+
+  await prisma.$disconnect().then(() => {
+    console.log('- Prisma disconnected.');
+  });
+
+  console.log('Done.');
+  process.exit();
+}
