0268f644654698e1e7e0d15fc3cb6d2f05878c95
Vlad Pavlenko <pavlenkoliar@gmail.com>
2024-01-28 23:36:19 +0200

    implement universal YAML wrapper

---
+++ src/data/YAMLWrapper.ts
@@ -0,0 +1,58 @@
+import YAML from "yaml";
+import fs from "fs";
+import { isNodeError } from "../types/typeGuards";
+import path from "path";
+
+class YAMLWrapper<T> {
+  readonly filename: string;
+  readonly filepath: string;
+  readonly dirrectory: string;
+  public data!: T;
+
+  constructor(filename: string, dirrectory: string) {
+    if (filename.includes("/")) {
+      throw new Error("Filename must be without path.");
+    }
+    if (dirrectory.includes(".")) {
+      throw new Error(
+        "Dirrectory must contain only path to the file dirrectory."
+      );
+    }
+    this.filename = filename.endsWith(".yaml") ? filename : filename + ".yaml";
+    this.dirrectory = dirrectory;
+    this.filepath = path.join(this.dirrectory, this.filename);
+    this.data = {} as T;
+  }
+
+  load() {
+    try {
+      this.data = YAML.parse(fs.readFileSync(this.filepath, "utf8"));
+    } catch (e: any) {
+      if (isNodeError(e) && e.code === "ENOENT") {
+        console.info(`${this.filepath} not found. Starting new one.`);
+        return;
+      } else {
+        throw e;
+      }
+    }
+    console.log(`${this.filename} loaded.`);
+  }
+
+  save() {
+    try {
+      fs.writeFileSync(this.filepath, YAML.stringify(this.data));
+    } catch (e: any) {
+      if (isNodeError(e) && e.code === "ENOENT") {
+        console.info(`${this.filepath} not found. Creating new one.`);
+
+        fs.mkdirSync(this.dirrectory, { recursive: true });
+        fs.writeFileSync(this.filepath, YAML.stringify(this.data));
+      } else {
+        throw new Error(e);
+      }
+    }
+    console.log(`${this.filename} saved.`);
+  }
+}
+
+export default YAMLWrapper;
