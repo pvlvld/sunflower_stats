c8f628420371c1295b0b42cf147a7b2d0c1cfa54
Vlad Pavlenko <pavlenkoliar@gmail.com>
2024-01-28 23:54:12 +0200

    init type guards. add isNodeError

---
+++ src/types/typeGuards.ts
@@ -0,0 +1,6 @@
+/**
+ * @param error the error object.
+ * @returns if given error object is a NodeJS error.
+ */
+export const isNodeError = (error: unknown): error is NodeJS.ErrnoException =>
+  error instanceof Error;
