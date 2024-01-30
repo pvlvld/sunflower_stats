2c4116fb5897b3545a4cdd677cdadc0fc88671ea
Vlad Pavlenko <pavlenkoliar@gmail.com>
2024-01-30 17:19:57 +0200

    feat: dynamic names
    make filename callback, filepath function

--- src/data/YAMLWrapper.ts
+++ src/data/YAMLWrapper.ts
@@ -4,32 +4,32 @@ import { isNodeError } from "../types/typeGuards";
 import path from "path";
 
 class YAMLWrapper<T> {
-  readonly filename: string;
-  readonly filepath: string;
+  readonly filename: () => string;
+  readonly filepath: () => string;
   readonly dirrectory: string;
   public data!: T;
 
-  constructor(filename: string, dirrectory: string) {
-    if (filename.includes("/")) {
-      throw new Error("Filename must be without path.");
+  constructor(filename: () => string, dirrectory: string) {
+    if (filename().includes("/") || filename().includes(".")) {
+      throw new Error("Filename must be without path and file extension.");
     }
     if (dirrectory.includes(".")) {
       throw new Error(
         "Dirrectory must contain only path to the file dirrectory."
       );
     }
-    this.filename = filename.endsWith(".yaml") ? filename : filename + ".yaml";
+    this.filename = filename;
     this.dirrectory = dirrectory;
-    this.filepath = path.join(this.dirrectory, this.filename);
+    this.filepath = () => path.join(this.dirrectory, this.filename());
     this.data = {} as T;
   }
 
   load() {
     try {
-      this.data = YAML.parse(fs.readFileSync(this.filepath, "utf8"));
+      this.data = YAML.parse(fs.readFileSync(this.filepath(), "utf8"));
     } catch (e: any) {
       if (isNodeError(e) && e.code === "ENOENT") {
-        console.info(`${this.filepath} not found. Starting new one.`);
+        console.info(`${this.filepath()} not found. Starting new one.`);
         return;
       } else {
         throw e;
@@ -40,13 +40,13 @@ class YAMLWrapper<T> {
 
   save() {
     try {
-      fs.writeFileSync(this.filepath, YAML.stringify(this.data));
+      fs.writeFileSync(this.filepath(), YAML.stringify(this.data));
     } catch (e: any) {
       if (isNodeError(e) && e.code === "ENOENT") {
-        console.info(`${this.filepath} not found. Creating new one.`);
+        console.info(`${this.filepath()} not found. Creating new one.`);
 
         fs.mkdirSync(this.dirrectory, { recursive: true });
-        fs.writeFileSync(this.filepath, YAML.stringify(this.data));
+        fs.writeFileSync(this.filepath(), YAML.stringify(this.data));
       } else {
         throw new Error(e);
       }
