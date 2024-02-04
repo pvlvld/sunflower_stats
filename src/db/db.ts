118a238632b00933f044e7c9e77cbaa343a66f99
Vlad Pavlenko <pavlenkoliar@gmail.com>
2024-02-04 20:05:37 +0200

    use namedPlacehonders

--- src/db/db.ts
+++ src/db/db.ts
@@ -34,6 +34,7 @@ class MySQLPoolManager {
 
 const DBPoolManager = new MySQLPoolManager({
   connectionLimit: 10,
+  namedPlaceholders: true,
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
