efeab9117672c0c0cf4ffa183a4527dac6741a87
Vlad Pavlenko <pavlenkoliar@gmail.com>
2024-02-05 01:26:31 +0200

    remove getFormattedRange

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -5,29 +5,22 @@ export const yyyy_mm_dd_date = () => {
 };
 
 export class DynamicDateRange {
-  private getFormattedDateRange(
-    start: moment.Moment,
-    end: moment.Moment
-  ): [string, string] {
-    return [start.format("YYYY-MM-DD"), end.format("YYYY-MM-DD")];
-  }
-
   get yesterdayDate(): string {
     return moment().subtract(1, "days").format("YYYY-MM-DD");
   }
 
   get weekRange(): [monday: string, sunday: string] {
-    const monday = moment().startOf("week");
-    const sunday = moment().endOf("week");
-
-    return this.getFormattedDateRange(monday, sunday);
+    return [
+      moment().startOf("week").format("YYYY-MM-DD"),
+      moment().endOf("week").format("YYYY-MM-DD"),
+    ];
   }
 
   get monthRange(): [firstDayOfTheMonth: string, lastDayOfTheMonth: string] {
-    const firstDay = moment().startOf("month");
-    const lastDay = moment().endOf("month");
-
-    return this.getFormattedDateRange(firstDay, lastDay);
+    return [
+      moment().startOf("month").format("YYYY-MM-DD"),
+      moment().endOf("month").format("YYYY-MM-DD"),
+    ];
   }
 }
 
