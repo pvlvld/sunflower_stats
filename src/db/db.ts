c4d64717597e9d41e306340c791986b18e1d2533
Vlad Pavlenko <pavlenkoliar@gmail.com>
2024-02-04 18:52:09 +0200

    rewrite to class

--- src/db/db.ts
+++ src/db/db.ts
@@ -10,16 +10,39 @@ if (
   throw new Error("Provide database env credentials.");
 }
 
-const db = mysql.createConnection({
+class MySql {
+  private config: mysql.ConnectionOptions;
+  private connection!: mysql.Connection;
+
+  constructor(config: mysql.ConnectionOptions) {
+    this.config = config;
+  }
+
+  async createConnection() {
+    if (this.connection) return;
+    this.connection = await mysql.createConnection(this.config);
+    await this.connection.query("SELECT 1");
+  }
+
+  get getConnection() {
+    if (!this.connection) {
+      throw new Error("Connection was not created.");
+    }
+    return this.connection;
+  }
+}
+
+const config = {
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
   charset: process.env.DB_CHARSET,
-});
+  connectionLimit: 10,
+};
 
-db.connect((err) => {
-  if (err) throw err;
-});
+export type IMySQL = typeof MySql;
 
-export default db;
+const MySQLConnector = new MySql(config);
+
+export default MySQLConnector;
