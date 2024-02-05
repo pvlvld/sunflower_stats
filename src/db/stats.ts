fee43b7ddddb3e83aa19a1cbd44de28ea757bf68
Vlad Pavlenko <pavlenkoliar@gmail.com>
2024-02-05 21:39:45 +0200

    add week method

--- src/db/stats.ts
+++ src/db/stats.ts
@@ -47,6 +47,25 @@ class DbChatStats {
       ];
     }
   }
+
+  async week(chat_id: number) {
+    try {
+      return (
+        await this.dbPool.query(
+          `SELECT user_id, CAST(SUM(count) as UNSIGNED) as count, name, username FROM stats_day_statistics WHERE chat_id = ${chat_id} AND date BETWEEN "${this.dateRange.weekRange[0]}" AND "${this.dateRange.weekRange[1]}" GROUP BY user_id, name, username`
+          // SELECT user_id, SUM(count), name, username FROM stats_day_statistics WHERE chat_id = %s AND date BETWEEN %s AND %s GROUP BY user_id
+        )
+      )[0] as [
+        { user_id: number; count: number; name: string; username: string }
+      ];
+    } catch (error) {
+      console.error(error);
+      //@ts-ignore
+      return [] as [
+        { user_id: number; count: number; name: string; username: string }
+      ];
+    }
+  }
 }
 
 class DbBotStats {
