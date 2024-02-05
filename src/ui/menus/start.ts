c1929076da32ffd2254d6a8ff68e3619875aa35e
Vlad Pavlenko <pavlenkoliar@gmail.com>
2024-02-05 20:35:27 +0200

    add start menu

---
+++ src/ui/menus/start.ts
@@ -0,0 +1,14 @@
+import { Menu } from "@grammyjs/menu";
+import { MyContext } from "../../types/context";
+
+const start_menu = new Menu<MyContext>("start-menu", {
+  autoAnswer: true,
+})
+  .url("Додати бота в чат", "https://t.me/soniashnyk_statistics_bot?startgroup")
+  .row()
+  .url(
+    "Підтримати існування соняха.",
+    "https://send.monobank.ua/jar/6TjRWExdMt"
+  );
+
+export default start_menu;
