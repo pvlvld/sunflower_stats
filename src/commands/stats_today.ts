a09f7cbaa4d748ffee839a9282effad2c72a24b4
Vlad Pavlenko <pavlenkoliar@gmail.com>
2024-02-05 18:33:58 +0200

    sort users by msg count

--- src/commands/stats_today.ts
+++ src/commands/stats_today.ts
@@ -6,23 +6,30 @@ import yamlStats from "../data/stats";
 async function stats_today(
   ctx: ChatTypeContext<MyContext, "supergroup" | "group">
 ) {
-  const stats = yamlStats.data?.[ctx.chat.id];
+  let stats = yamlStats.data?.[ctx.chat.id];
+  if (!stats || stats === undefined) return;
 
   let reply = "📊 Статистика чату за сьогодні:\n\n";
   let totlal_messages = 0;
-  let members_in_stats = 0;
+  let stats_s = [] as any[],
+    b;
 
-  for (const key in stats) {
-    if (members_in_stats >= 50) break;
-    members_in_stats++;
+  for (b in stats) {
+    stats_s.push({ ...stats[b], user_id: b });
+  }
 
-    reply += `${members_in_stats}\\. ${getUserNameLink.markdown(
-      stats[key]?.name as string,
-      stats[key]?.username,
-      key
-    )} — ${stats[key]?.all}\n`;
+  stats_s = stats_s.sort((a, b) => {
+    return a.day < b.day ? 1 : -1;
+  });
 
-    totlal_messages += stats[key]?.all || 0;
+  for (let i = 0; i < Math.min(50, stats_s.length); i++) {
+    reply += `${i + 1}\\. ${getUserNameLink.markdown(
+      stats_s[i].name,
+      stats_s[i].username,
+      stats_s[i].user_id
+    )} — ${stats_s[i].day || 0}\n`;
+
+    totlal_messages += stats_s[i].day || 0;
   }
 
   reply += `\nЗагальна кількість повідомлень: ${totlal_messages}`;
