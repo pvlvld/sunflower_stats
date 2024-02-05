b1fe09b2e08b4db13647680da123199ac496632c
Vlad Pavlenko <pavlenkoliar@gmail.com>
2024-02-06 00:06:27 +0200

    make main fn

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,10 @@ import * as http from "http";
 import process from "node:process";
 import Stats from "./data/stats";
 import Active from "./data/active";
+import DBPoolManager from "./db/db";
+import DbStats from "./db/stats";
+import regCommands from "./commands";
+import DateRange from "./utils/date";
 // import { getPublicIP } from './utils/getPublicIP';
 // import { startStatsCollecting } from './utils/statsCollector';
 
@@ -31,69 +35,76 @@ bot.catch((err) => {
   }
 });
 
-let server: http.Server = {} as http.Server;
-
-if (process.env.NODE_ENV === "production") {
+async function main() {
+  let server: http.Server = {} as http.Server;
   loadData();
-  // const app = http.createServer(webhookCallback(bot, "http"));
-  // server = app.listen(Number(process.env.PORT ?? 8443));
-  // bot.api.deleteWebhook({ drop_pending_updates: true }).then(async () => {
-  //   await bot.api
-  //     .setWebhook(
-  //       `https://${await getPublicIP()}/${String(process.env.BOT_TOKEN)}`
-  //     )
-  //     .catch((e) => {
-  //       throw new Error(e);
-  //     });
-  // });
-} else if (process.env.NODE_ENV === "test") {
-  loadData();
-  const app = http.createServer(webhookCallback(bot, "http"));
-  server = app.listen(Number(process.env.PORT ?? 8443));
+  await DBPoolManager.createPool();
+  const dbStats = new DbStats(DBPoolManager.getPool, DateRange);
+  regCommands(dbStats);
+  // const [test, str] = await DBPoolManager.getPool.query(
+  //   "SELECT user_id, count, name, username FROM stats_day_statistics WHERE date = '2024-02-02' AND chat_id = -1001898242958 ORDER BY count DESC"
+  // );
+  if (process.env.NODE_ENV === "production") {
+    // const app = http.createServer(webhookCallback(bot, "http"));
+    // server = app.listen(Number(process.env.PORT ?? 8443));
+    // bot.api.deleteWebhook({ drop_pending_updates: true }).then(async () => {
+    //   await bot.api
+    //     .setWebhook(
+    //       `https://${await getPublicIP()}/${String(process.env.BOT_TOKEN)}`
+    //     )
+    //     .catch((e) => {
+    //       throw new Error(e);
+    //     });
+    // });
+  } else if (process.env.NODE_ENV === "test") {
+    const app = http.createServer(webhookCallback(bot, "http"));
+    server = app.listen(Number(process.env.PORT ?? 8443));
 
-  console.log("Started in test mode");
-} else {
-  loadData();
-  bot.api.deleteWebhook({ drop_pending_updates: true }).then(() => {
-    bot.start({
-      drop_pending_updates: true,
-      allowed_updates: [
-        "message",
-        "my_chat_member",
-        "chat_member",
-        "callback_query",
-      ],
-    });
+    console.log("Started in test mode");
+  } else {
+    bot.api.deleteWebhook({ drop_pending_updates: true }).then(() => {
+      bot.start({
+        drop_pending_updates: true,
+        allowed_updates: [
+          "message",
+          "my_chat_member",
+          "chat_member",
+          "callback_query",
+        ],
+      });
 
-    // startStatsCollecting();
-    console.log("Bot is started.");
-  });
-}
-
-process.on("SIGINT", async () => await shutdown());
-process.on("SIGTERM", async () => await shutdown());
-
-let shutdownCounter = 0;
-
-async function shutdown() {
-  if (shutdownCounter) return;
-  shutdownCounter++;
-
-  console.log("Shutting down.");
-  await bot.api.deleteWebhook().then(() => {
-    console.log("Webhook removed");
-  });
-
-  await bot.stop().then(() => {
-    console.log("- Bot stopped.");
-  });
-
-  if ("close" in server) {
-    server.close(() => {
-      console.log("- Server closed.");
+      // startStatsCollecting();
+      console.log("Bot is started.");
     });
   }
 
-  console.log("Done.");
-  process.exit();
+  process.on("SIGINT", async () => await shutdown());
+  process.on("SIGTERM", async () => await shutdown());
+
+  let shutdownCounter = 0;
+
+  async function shutdown() {
+    if (shutdownCounter) return;
+    shutdownCounter++;
+
+    console.log("Shutting down.");
+    await bot.api.deleteWebhook().then(() => {
+      console.log("Webhook removed");
+    });
+
+    await bot.stop().then(() => {
+      console.log("- Bot stopped.");
+    });
+
+    if ("close" in server) {
+      server.close(() => {
+        console.log("- Server closed.");
+      });
+    }
+
+    console.log("Done.");
+    process.exit();
+  }
 }
+
+main();
