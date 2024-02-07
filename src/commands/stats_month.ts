2f9761402dce27c24739037a8412a798b0dfd650
Vlad Pavlenko <pavlenkoliar@gmail.com>
2024-02-07 21:21:50 +0200

    use getStatsRatingPlusToday

--- src/commands/stats_month.ts
+++ src/commands/stats_month.ts
@@ -2,9 +2,8 @@ import type { MyContext } from "../types/context";
 import { type ChatTypeContext, type HearsContext } from "grammy";
 import DbStats from "../db/stats";
 import isDbResNotEmpty from "../utils/isDbResNotEmpty";
-import getUserNameLink from "../utils/getUserNameLink";
 import YAMLStats from "../data/stats";
-import addTodayUserMessages from "../utils/addTodayUserMessages";
+import { getStatsRatingPlusToday } from "../utils/getStatsRating";
 
 async function stats_month(
   ctx: HearsContext<ChatTypeContext<MyContext, "supergroup" | "group">>,
@@ -17,32 +16,15 @@ async function stats_month(
     return;
   }
 
-  let reply = "📊 Статистика чату за цей місяць:\n\n";
-  let totlal_messages = 0;
-
-  for (let i = 0; i < Math.min(50, stats.length); i++) {
-    const totalUserMessages = addTodayUserMessages(
-      ctx.chat.id,
-      ctx.from.id,
-      stats[i].count || 0,
-      yamlStats
-    );
-    reply += `${i + 1}\\. ${getUserNameLink.markdown(
-      stats[i].name,
-      stats[i].username,
-      stats[i].user_id
-    )} — ${totalUserMessages}\n`;
-
-    totlal_messages += totalUserMessages;
-  }
-
-  reply += `\nЗагальна кількість повідомлень: ${totlal_messages}`;
-
-  ctx.reply(reply, {
-    parse_mode: "MarkdownV2",
-    disable_notification: true,
-    link_preview_options: { is_disabled: true },
-  });
+  ctx.reply(
+    "📊 Статистика чату за цей місяць:\n\n" +
+      getStatsRatingPlusToday(stats, ctx.chat.id, ctx.from.id, yamlStats),
+    {
+      parse_mode: "MarkdownV2",
+      disable_notification: true,
+      link_preview_options: { is_disabled: true },
+    }
+  );
 }
 
 export default stats_month;
--- src/commands/stats_week.ts
+++ src/commands/stats_week.ts
@@ -1,40 +1,30 @@
 import type { MyContext } from "../types/context";
-import type { ChatTypeContext } from "grammy";
+import type { ChatTypeContext, HearsContext } from "grammy";
 import DbStats from "../db/stats";
 import isDbResNotEmpty from "../utils/isDbResNotEmpty";
-import getUserNameLink from "../utils/getUserNameLink";
+import { getStatsRatingPlusToday } from "../utils/getStatsRating";
+import YAMLStats from "../data/stats";
 
 async function stats_week(
-  ctx: ChatTypeContext<MyContext, "supergroup" | "group">,
-  dbStats: DbStats
+  ctx: HearsContext<ChatTypeContext<MyContext, "supergroup" | "group">>,
+  dbStats: DbStats,
+  yamlStats: YAMLStats
 ) {
   const stats = await dbStats.chat.week(ctx.chat.id);
-  // TODO: adding today stats from yaml
   if (!isDbResNotEmpty(stats)) {
     ctx.reply("Щось пішло не так.");
     return;
   }
 
-  let reply = "📊 Статистика чату за цей тиждень:\n\n";
-  let totlal_messages = 0;
-
-  for (let i = 0; i < Math.min(50, stats.length); i++) {
-    reply += `${i + 1}\\. ${getUserNameLink.markdown(
-      stats[i].name,
-      stats[i].username,
-      stats[i].user_id
-    )} — ${stats[i].count || 0}\n`;
-
-    totlal_messages += stats[i].count || 0;
-  }
-
-  reply += `\nЗагальна кількість повідомлень: ${totlal_messages}`;
-
-  ctx.reply(reply, {
-    parse_mode: "MarkdownV2",
-    disable_notification: true,
-    link_preview_options: { is_disabled: true },
-  });
+  ctx.reply(
+    "📊 Статистика чату за цей тиждень:\n\n" +
+      getStatsRatingPlusToday(stats, ctx.chat.id, ctx.from.id, yamlStats),
+    {
+      parse_mode: "MarkdownV2",
+      disable_notification: true,
+      link_preview_options: { is_disabled: true },
+    }
+  );
 }
 
 export default stats_week;
