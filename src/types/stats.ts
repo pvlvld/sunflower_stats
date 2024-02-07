4fce94037f206b070ffcc510e3bc7419bc8056ad
Vlad Pavlenko <pavlenkoliar@gmail.com>
2024-02-07 17:47:12 +0200

    update IStats

--- src/types/stats.ts
+++ src/types/stats.ts
@@ -1,26 +1,23 @@
-export type IStatsMessageTypes = {
-  text: number;
-  voice: number;
-  sticker: number;
-  file: number;
-  video: number;
-  gif: number;
-  image: number;
-  photo: number;
-};
+// export type IStatsMessageTypes = {
+//   text: number;
+//   voice: number;
+//   sticker: number;
+//   file: number;
+//   video: number;
+//   gif: number;
+//   image: number;
+//   photo: number;
+// };
 
 export interface IStats {
   [chat_id: string]:
     | {
         [user_id: string]:
-          | ({
+          | {
               name: string;
-              username: string;
-              day: number;
-              week: number;
-              month: number;
-              all: number;
-            } & IStatsMessageTypes)
+              username: string | undefined;
+              messages: number;
+            }
           | undefined;
       }
     | undefined;
