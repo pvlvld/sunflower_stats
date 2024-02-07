aec947f2ae7c954e32137389e714833f73d7300d
Vlad Pavlenko <pavlenkoliar@gmail.com>
2024-02-07 21:22:43 +0200

    init stats year cmd

---
+++ src/commands/stats_year.ts
@@ -0,0 +1,30 @@
+import type { MyContext } from "../types/context";
+import { type ChatTypeContext, type HearsContext } from "grammy";
+import DbStats from "../db/stats";
+import isDbResNotEmpty from "../utils/isDbResNotEmpty";
+import YAMLStats from "../data/stats";
+import { getStatsRatingPlusToday } from "../utils/getStatsRating";
+
+async function stats_year(
+  ctx: HearsContext<ChatTypeContext<MyContext, "supergroup" | "group">>,
+  dbStats: DbStats,
+  yamlStats: YAMLStats
+) {
+  const stats = await dbStats.chat.year(ctx.chat.id);
+  if (!isDbResNotEmpty(stats)) {
+    ctx.reply("Щось пішло не так.");
+    return;
+  }
+
+  ctx.reply(
+    "📊 Статистика чату за цей рік:\n\n" +
+      getStatsRatingPlusToday(stats, ctx.chat.id, ctx.from.id, yamlStats),
+    {
+      parse_mode: "MarkdownV2",
+      disable_notification: true,
+      link_preview_options: { is_disabled: true },
+    }
+  );
+}
+
+export default stats_year;
