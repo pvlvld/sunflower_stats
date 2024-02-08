68b210f5e88982586e7749474d44f8e3144e1373
Vlad Pavlenko <pavlenkoliar@gmail.com>
2024-02-08 17:57:12 +0200

    init stats_my cmd

---
+++ src/commands/stats_my.ts
@@ -0,0 +1,32 @@
+import type { MyContext } from "../types/context";
+import type { ChatTypeContext, HearsContext } from "grammy";
+import DbStats from "../db/stats";
+import YAMLStats from "../data/stats";
+import YAMLWrapper from "../data/YAMLWrapper";
+import { IActive } from "../data/active";
+import escapeMarkdownV2 from "../utils/escapeMarkdownV2";
+import getUserStatsMessage from "../utils/getUserStatsMessage";
+
+async function stats_my(
+  ctx: HearsContext<ChatTypeContext<MyContext, "supergroup" | "group">>,
+  dbStats: DbStats,
+  yamlStats: YAMLStats,
+  active: YAMLWrapper<IActive>
+) {
+  ctx.reply(
+    getUserStatsMessage(
+      ctx.chat.id,
+      ctx.from,
+      await dbStats.user.all(ctx.chat.id, ctx.from.id),
+      yamlStats,
+      active
+    ),
+    {
+      parse_mode: "MarkdownV2",
+      disable_notification: true,
+      link_preview_options: { is_disabled: true },
+    }
+  );
+}
+
+export default stats_my;
