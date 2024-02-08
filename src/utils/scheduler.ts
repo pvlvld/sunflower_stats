383ab066f5c05fee301d1f050fdb0d681b0afd5a
Vlad Pavlenko <pavlenkoliar@gmail.com>
2024-02-08 17:56:57 +0200

    init scheduler

---
+++ src/utils/scheduler.ts
@@ -0,0 +1,31 @@
+import * as cron from "node-cron";
+import YAMLWrapper from "../data/YAMLWrapper";
+import { IActive } from "../data/active";
+import YAMLStats from "../data/stats";
+import path from "path";
+import formattedDate from "./date";
+
+function createScheduler(active: YAMLWrapper<IActive>, yamlStats: YAMLStats) {
+  return cron.schedule(
+    "0 * * * *", //Every hour at 00 minutes
+    (date) => {
+      if (date instanceof Date && date.getHours() === 0) {
+        active.save(
+          path.join("data/active", `active-${formattedDate.today}.yaml`)
+        );
+        yamlStats.clear(); // Clearing local today stats on midnight
+      }
+
+      active.save();
+      yamlStats.save();
+    },
+    {
+      timezone: "Europe/Kiev",
+    }
+  );
+}
+
+export default createScheduler;
+// save local
+// write to db
+// clear
