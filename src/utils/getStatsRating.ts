e3f7c33e19778317bd48999a5d96a7cae6b8a942
Vlad Pavlenko <pavlenkoliar@gmail.com>
2024-02-10 00:56:29 +0200

    remove unused getStatsRating

--- src/utils/getStatsRating.ts
+++ src/utils/getStatsRating.ts
@@ -3,26 +3,6 @@ import { IDbChatUserStats, IStats } from "../types/stats";
 import addTodayUserMessages from "./addTodayUserMessages";
 import getUserNameLink from "./getUserNameLink";
 
-export function getStatsRating(stats: any) {
-  let reply = "";
-  let totalChatMessages = 0;
-
-  for (let i = 0; i < Math.min(50, stats.length); i++) {
-    reply += `${i + 1}\\. ${getUserNameLink.markdown(
-      stats[i].name,
-      stats[i].username,
-      stats[i].user_id
-    )} — ${stats[i].messages || 0}\n`;
-
-    totalChatMessages += stats[i].day || 0;
-  }
-
-  reply += `\nЗагальна кількість повідомлень: ${totalChatMessages}`;
-
-  return reply;
-}
-
-// Separate function to reduce ticks due to infrequent use of db stats without adding today stats
 export function getStatsRatingPlusToday(
   stats: IDbChatUserStats[],
   chat_id: number,
