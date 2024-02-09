63614407b3d0671829cb955dc6cfffc5b9328cf0
Vlad Pavlenko <pavlenkoliar@gmail.com>
2024-02-10 00:53:57 +0200

    bruh

--- src/utils/addTodayUserMessages.ts
+++ src/utils/addTodayUserMessages.ts
@@ -6,11 +6,7 @@ function addTodayUserMessages(
   messages: number,
   yamlStats: YAMLStats
 ) {
-  if (yamlStats.data[chat_id]?.[user_id]) {
-    //@ts-ignore
-    return messages + yamlStats.data[chat_id][user_id].messages;
-  }
-  return messages;
+  return messages + (yamlStats.data[chat_id]?.[user_id] || 0);
 }
 
 export default addTodayUserMessages;
