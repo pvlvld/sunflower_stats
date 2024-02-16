65a7f3e1fc18134a58da2d95304562f694cd01d5
Vlad Pavlenko <pavlenkoliar@gmail.com>
2024-02-16 16:25:31 +0200

    init chatInactive_cmd

---
+++ src/commands/chat_inactive.ts
@@ -0,0 +1,77 @@
+import YAMLWrapper from "../data/YAMLWrapper";
+import IActive from "../data/active";
+import { type MyContext } from "../types/context";
+import Escape from "../utils/escape";
+import parseCmdArgs from "../utils/parseCmdArgs";
+import { type ChatTypeContext, type HearsContext, type Filter } from "grammy";
+
+const PAGE_LENGTH = 25;
+
+function chatInactive_cmd(
+  ctx: Filter<
+    HearsContext<ChatTypeContext<MyContext, "supergroup" | "group">>,
+    ":text"
+  >,
+  active: YAMLWrapper<IActive>
+) {
+  const page = parseInt(parseCmdArgs(ctx.msg.text)[0]);
+  if (!page) return ctx.reply("Введіть номер сторінки.\n!неактив 1");
+
+  ctx.reply(getInactivePageMessage(ctx.chat.id, Math.abs(page), active), {
+    parse_mode: "Markdown",
+  });
+}
+
+function getInactivePageMessage(
+  chat_id: number,
+  page: number,
+  active: YAMLWrapper<IActive>
+) {
+  const inactiveUsers = getInactivePage(chat_id, page, active);
+  if (inactiveUsers.length === 0) return "Ця сторінка порожня\\.";
+
+  return inactiveUsers
+    .map(
+      (user, i) =>
+        `${i + 1 + (page - 1) * PAGE_LENGTH}\\. ${genUserPageRecord(
+          chat_id,
+          user,
+          active
+        )}`
+    )
+    .join("\n");
+}
+
+function genUserPageRecord(
+  chat_id: number,
+  user: string,
+  active: YAMLWrapper<IActive>
+) {
+  return `**${Escape.markdownV1(
+    active.data[chat_id]?.[user]?.name || "невідомо"
+  )}** — ${Escape.markdownV1(
+    active.data[chat_id]?.[user]?.active_last || "невідомо"
+  )}`;
+}
+
+function getInactivePage(
+  chat_id: number,
+  page: number,
+  active: YAMLWrapper<IActive>
+) {
+  return getSortedInactive(chat_id, active).slice(
+    PAGE_LENGTH * (page - 1),
+    PAGE_LENGTH * (page - 1) + PAGE_LENGTH
+  );
+}
+
+function getSortedInactive(chat_id: number, active: YAMLWrapper<IActive>) {
+  return Object.keys(active.data?.[chat_id] || {}).sort((u1, u2) => {
+    return (active.data?.[chat_id]?.[u1]?.active_last || 0) >
+      (active.data?.[chat_id]?.[u2]?.active_last || 0)
+      ? 1
+      : -1;
+  });
+}
+
+export default chatInactive_cmd;
