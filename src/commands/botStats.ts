a21be90ade62a54faec01fbd2462c945a89a23bb
Vlad Pavlenko <pavlenkoliar@gmail.com>
2024-02-18 01:59:36 +0200

    add newGroups counter

--- src/commands/botStats.ts
+++ src/commands/botStats.ts
@@ -2,20 +2,22 @@ import { MyContext } from "../types/context";
 
 type IBotStats = {
   commands: { [key: string]: number };
+  newGroups: number;
 };
 
 const BOT_STATS: IBotStats = {
   commands: {},
+  newGroups: 0,
 };
 
 function bot_stats_cmd(ctx: MyContext) {
-  let cmdUsageMsg = "";
+  let statsMsg = `Нових чатів: ${BOT_STATS.newGroups}\n\n`;
 
   for (const cmd in BOT_STATS.commands) {
-    cmdUsageMsg += `${cmd} - ${BOT_STATS.commands[cmd]}\n`;
+    statsMsg += `${cmd} - ${BOT_STATS.commands[cmd]}\n`;
   }
 
-  ctx.reply(cmdUsageMsg);
+  ctx.reply(statsMsg);
 }
 
 export const collectBotStats = {
@@ -23,6 +25,7 @@ export const collectBotStats = {
     BOT_STATS.commands[cmd] ??= 0;
     BOT_STATS.commands[cmd]++;
   },
+  newGroups: () => BOT_STATS.newGroups++,
 };
 
 export default bot_stats_cmd;
