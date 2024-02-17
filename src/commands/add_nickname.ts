373ae9576388da8321ec0e8d4e2f8ab63befce7c
Vlad Pavlenko <pavlenkoliar@gmail.com>
2024-02-18 00:17:53 +0200

    refactor & limit nickname length

--- src/commands/add_nickname.ts
+++ src/commands/add_nickname.ts
@@ -2,20 +2,23 @@ import type { MyContext } from "../types/context";
 import { type ChatTypeContext, type HearsContext } from "grammy";
 import YAMLWrapper from "../data/YAMLWrapper";
 import IActive from "../data/active";
+import parseCmdArgs from "../utils/parseCmdArgs";
 
 async function add_nickname(
-    ctx: HearsContext<ChatTypeContext<MyContext, "supergroup" | "group">>,
-    active: YAMLWrapper<IActive>
+  ctx: HearsContext<ChatTypeContext<MyContext, "supergroup" | "group">>,
+  active: YAMLWrapper<IActive>
 ) {
-    const result = ctx.message?.text?.match(/^((\+нік|\+нікнейм) ((.*)))$/i);
-    console.log(result);
-    const nickname = result?.[3];
-    //@ts-expect-error
-    active.data[ctx.chat.id][ctx.from.id].nickname = nickname;
-    ctx.reply(`✅ Успішно встановлено нікнейм: ${nickname}`, {
-        parse_mode: "MarkdownV2",
-        disable_notification: true,
-        link_preview_options: { is_disabled: true },
-    });
+  const nickname = parseCmdArgs(ctx.msg.text as string).join(" ");
+  if (nickname.length > 20) {
+    ctx.reply("Максимальна довжина нікнейму 20 символів.");
+    return;
+  }
+
+  //@ts-expect-error
+  active.data[ctx.chat.id][ctx.from.id].nickname = nickname;
+  ctx.reply(`✅ Успішно встановлено нікнейм: ${nickname}`, {
+    disable_notification: true,
+    link_preview_options: { is_disabled: true },
+  });
 }
 export default add_nickname;
