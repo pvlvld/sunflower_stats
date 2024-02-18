8ea4519e3b94894a1bc2e972774ab74b9a9bec27
Vlad Pavlenko <pavlenkoliar@gmail.com>
2024-02-18 02:00:09 +0200

    collect new groups stats

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -3,10 +3,15 @@ import leaveChatMemberHandler from "./leaveChatMember";
 import YAMLStats from "../data/stats";
 import YAMLWrapper from "../data/YAMLWrapper";
 import IActive from "../data/active";
+import { collectBotStats } from "../commands/botStats";
 
 function regHandlers(active: YAMLWrapper<IActive>, yamlStats: YAMLStats) {
   bot.on(":left_chat_member", (ctx) => {
     leaveChatMemberHandler(ctx, yamlStats, active);
+  });
+
+  bot.on("msg:new_chat_members:me", (ctx) => {
+    collectBotStats.newGroups();
   });
 }
 
