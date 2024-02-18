3ea1b76f37ae8a0d06920124ebf367d5df878d65
Vlad Pavlenko <pavlenkoliar@gmail.com>
2024-02-18 03:30:30 +0200

    add messages counter & rename collectBotStats -> botStatsManager

--- src/commands/botStats.ts
+++ src/commands/botStats.ts
@@ -3,11 +3,19 @@ import { MyContext } from "../types/context";
 type IBotStats = {
   commands: { [key: string]: number };
   newGroups: number;
+  messages: {
+    total: number;
+    start_count_date: Date;
+  };
 };
 
 const BOT_STATS: IBotStats = {
   commands: {},
   newGroups: 0,
+  messages: {
+    total: 0,
+    start_count_date: new Date(),
+  },
 };
 
 function bot_stats_cmd(ctx: MyContext) {
@@ -20,12 +28,22 @@ function bot_stats_cmd(ctx: MyContext) {
   ctx.reply(statsMsg);
 }
 
-export const collectBotStats = {
-  command: (cmd: string) => {
+export const botStatsManager = {
+  commandUse: (cmd: string) => {
     BOT_STATS.commands[cmd] ??= 0;
     BOT_STATS.commands[cmd]++;
   },
-  newGroups: () => BOT_STATS.newGroups++,
+  newGroup: () => BOT_STATS.newGroups++,
+  newMessage: () => BOT_STATS.messages.total++,
+  resetMessages: () => {
+    BOT_STATS.messages.total = 0;
+    BOT_STATS.messages.start_count_date = new Date();
+  },
+  resetAll: () => {
+    botStatsManager.resetMessages();
+    BOT_STATS.commands = {};
+    BOT_STATS.newGroups = 0;
+  },
 };
 
 export default bot_stats_cmd;
--- src/commands/index.ts
+++ src/commands/index.ts
@@ -18,7 +18,7 @@ import stats_year from "./stats_year";
 import stats_yestarday from "./stats_yesterday";
 import set_nickname from "./set_nickname";
 import del_nickname from "./del_nickname";
-import bot_stats_cmd, { collectBotStats } from "./botStats";
+import bot_stats_cmd, { botStatsManager } from "./botStats";
 
 function regCommands(
   dbStats: DbStats,
@@ -26,83 +26,83 @@ function regCommands(
   yamlStats: YAMLStats
 ) {
   bot.command("help", async (ctx) => {
-    collectBotStats.command("help");
+    botStatsManager.commandUse("help");
     help_cmd(ctx);
   });
 
   bot.command("start", async (ctx) => {
-    collectBotStats.command("start");
+    botStatsManager.commandUse("start");
     start_cmd(ctx);
   });
 
   bot
     .chatType(["group", "supergroup"])
     .hears(/^(статистика|стата) вчора$/i, async (ctx) => {
-      collectBotStats.command("стата вчора");
+      botStatsManager.commandUse("стата вчора");
       stats_yestarday(ctx, dbStats);
     });
 
   bot
     .chatType(["group", "supergroup"])
     .hears(/^(статистика|стата)\s*(сьогодні|день)?$/i, async (ctx) => {
-      collectBotStats.command("стата сьогодні");
+      botStatsManager.commandUse("стата сьогодні");
       stats_today(ctx, yamlStats, active);
     });
 
   bot
     .chatType(["group", "supergroup"])
     .hears(/^(статистика|стата) тиждень$/i, async (ctx) => {
-      collectBotStats.command("стата тиждень");
+      botStatsManager.commandUse("стата тиждень");
       stats_week(ctx, dbStats, yamlStats, active);
     });
 
   bot
     .chatType(["group", "supergroup"])
     .hears(/^(статистика|стата) місяць$/i, async (ctx) => {
-      collectBotStats.command("стата місяць");
+      botStatsManager.commandUse("стата місяць");
       stats_month(ctx, dbStats, yamlStats, active);
     });
 
   bot
     .chatType(["group", "supergroup"])
     .hears(/^(статистика|стата) рік$/i, async (ctx) => {
-      collectBotStats.command("стата рік");
+      botStatsManager.commandUse("стата рік");
       stats_year(ctx, dbStats, yamlStats, active);
     });
 
   bot
     .chatType(["group", "supergroup"])
     .hears(/^(статистика|стата) вся$/i, async (ctx) => {
-      collectBotStats.command("стата вся");
+      botStatsManager.commandUse("стата вся");
       stats_all(ctx, dbStats, yamlStats, active);
     });
 
   bot.chatType(["group", "supergroup"]).hears(/^(!я|!йа)$/i, async (ctx) => {
-    collectBotStats.command("я");
+    botStatsManager.commandUse("я");
     stats_my(ctx, dbStats, yamlStats, active);
   });
 
   bot.chatType(["group", "supergroup"]).hears(/^(!ти)$/i, async (ctx) => {
-    collectBotStats.command("ти");
+    botStatsManager.commandUse("ти");
     stats_their(ctx, dbStats, yamlStats, active);
   });
 
   bot
     .chatType(["group", "supergroup"])
     .hears(/^(\+нік|\+нікнейм)/i, async (ctx) => {
-      collectBotStats.command("нік");
+      botStatsManager.commandUse("нік");
       set_nickname(ctx, active);
     });
 
   bot
     .chatType(["group", "supergroup"])
     .hears(/^(\-нік|\-нікнейм)/i, async (ctx) => {
-      collectBotStats.command("нік");
+      botStatsManager.commandUse("нік");
       del_nickname(ctx, active);
     });
 
   bot.chatType(["group", "supergroup"]).hears(/^(!інактив)/i, async (ctx) => {
-    collectBotStats.command("інактив");
+    botStatsManager.commandUse("інактив");
     //@ts-expect-error
     chatInactive_cmd(ctx, active);
   });
