ce2c9f21db70624709cc776b868bbb0371f1cffe
Vlad Pavlenko <pavlenkoliar@gmail.com>
2024-02-19 21:15:33 +0200

    add getStatsRatingToday & use it when db stats is empty

--- src/utils/getStatsRating.ts
+++ src/utils/getStatsRating.ts
@@ -10,6 +10,10 @@ export function getStatsRatingPlusToday(
   todayStats: TodayStats,
   active: YAMLWrapper<IActive>
 ) {
+  if (stats.length === 0) {
+    return getStatsRatingToday(chat_id, todayStats, active);
+  }
+
   let reply = "";
   let totalChatMessages = 0;
 
@@ -39,4 +43,37 @@ export function getStatsRatingPlusToday(
   )}`;
 
   return reply;
+}
+
+export function getStatsRatingToday(
+  chat_id: number,
+  todayStats: TodayStats,
+  active: YAMLWrapper<IActive>
+) {
+  const stats = todayStats.data[chat_id];
+  if (!stats || stats === undefined) return;
+
+  let totlal_messages = 0;
+
+  const usersId_sorted = Object.keys(stats).sort((u1, u2) => {
+    //@ts-expect-error
+    return stats[u1] < stats[u2] ? 1 : -1;
+  });
+
+  let reply = "";
+
+  for (let i = 0; i < Math.min(50, usersId_sorted.length); i++) {
+    const user_id = usersId_sorted[i];
+    reply += `${i + 1}. ${getUserNameLink.html(
+      active.data[chat_id]?.[user_id]?.nickname ||
+        active.data[chat_id]?.[user_id]?.name ||
+        "Невідомо",
+      active.data[chat_id]?.[user_id]?.username,
+      user_id
+    )} — ${stats[user_id] || 0}\n`;
+
+    totlal_messages += stats[user_id] || 0;
+  }
+
+  return (reply += `\nЗагальна кількість повідомлень: ${totlal_messages}`);
 }
