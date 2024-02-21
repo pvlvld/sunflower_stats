47b7c8a036dfbf8b25b179b7e30d88ad606abec5
Vlad Pavlenko <pavlenkoliar@gmail.com>
2024-02-21 22:52:52 +0200

    add IFormattedRangeDateGetters

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,5 +1,11 @@
 import moment from "moment";
 
+export interface IFormattedRangeDateGetters {
+  weekRange: [monday: string, sunday: string];
+  monthRange: [firstDayOfTheMonth: string, lastDayOfTheMonth: string];
+  yearRange: [firstDayOfYear: string, lastDayOfYear: string];
+}
+
 /** Dates in "YYY-MM-DD" format.*/
 export class FormattedDate {
   get today(): string {
@@ -9,21 +15,21 @@ export class FormattedDate {
     return moment().subtract(1, "days").format("YYYY-MM-DD");
   }
 
-  get weekRange(): [monday: string, sunday: string] {
+  get weekRange(): IFormattedRangeDateGetters["weekRange"] {
     return [
       moment().startOf("isoWeek").format("YYYY-MM-DD"),
       moment().endOf("isoWeek").format("YYYY-MM-DD"),
     ];
   }
 
-  get monthRange(): [firstDayOfTheMonth: string, lastDayOfTheMonth: string] {
+  get monthRange(): IFormattedRangeDateGetters["monthRange"] {
     return [
       moment().startOf("month").format("YYYY-MM-DD"),
       moment().endOf("month").format("YYYY-MM-DD"),
     ];
   }
 
-  get yearRange(): [firstDayOfYear: string, lastDayOfYear: string] {
+  get yearRange(): IFormattedRangeDateGetters["yearRange"] {
     return [
       moment().startOf("year").format("YYYY-MM-DD"),
       moment().endOf("year").format("YYYY-MM-DD"),
