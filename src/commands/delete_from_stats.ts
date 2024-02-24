10d1023b9d6cee9b5df52b04aaaed03fe074ea5b
Vlad Pavlenko <pavlenkoliar@gmail.com>
2024-02-24 19:18:44 +0200

    refactor

--- src/commands/delete_from_stats.ts
+++ src/commands/delete_from_stats.ts
@@ -1,27 +1,32 @@
 import { MyContext } from "../types/context";
 import YAMLWrapper from "../data/YAMLWrapper";
 import IActive from "../data/active";
+import { CommandContext } from "grammy";
 
-async function delete_from_stats(ctx: MyContext, active: YAMLWrapper<IActive>) {
-    const chatMember = await ctx.getChatMember(ctx.from?.id || -1)
-    if (!chatMember || chatMember.status != 'creator') {
-        ctx.reply("❌ Щось пішло не так або ви не є власником чату");
-        return
-    } else {
-        //@ts-expect-error
-        if (active.data[ctx.chat.id]?.[ctx.message.reply_to_message.from.id]) {
-            //@ts-expect-error
-            const name = active.data[ctx.chat.id][ctx.message.reply_to_message.from.id].name
-            //@ts-expect-error
-            delete active.data[ctx.chat.id][ctx.message.reply_to_message.from.id]
-            ctx.reply(`✅ Успішно видалено ${name} з активу та приховано зі статистики.`)
-        } else {
-            ctx.reply("❌ Цього користувача немає в активі.")
-        }
-    }
-
+async function delete_from_stats(
+  ctx: CommandContext<MyContext>,
+  active: YAMLWrapper<IActive>
+) {
+  const chatMember = await ctx
+    .getChatMember(ctx.from?.id || -1)
+    .catch((e) => {});
+  if (!chatMember || chatMember.status != "creator") {
+    ctx.reply("❌ Щось пішло не так або ви не є власником чату");
+    return;
+  }
+  if (
+    ctx.msg.reply_to_message?.from?.id &&
+    active.data[ctx.chat.id]?.[ctx.msg.reply_to_message.from.id]
+  ) {
+    delete active.data[ctx.chat.id]?.[ctx.msg.reply_to_message.from.id];
+    ctx.reply(
+      `✅ Успішно видалено ${
+        active.data[ctx.chat.id]?.[ctx.msg.reply_to_message.from.id]?.name
+      } з активу та приховано зі статистики.`
+    );
+    return;
+  }
+  ctx.reply("❌ Цього користувача немає в активі.");
 }
 
 export default delete_from_stats;
-
-
