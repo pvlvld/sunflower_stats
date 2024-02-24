e3fbc5e7aa246d63ff0390036213941c7a892c74
uliiiiiiii <56516009+uliiiiiiii@users.noreply.github.com>
2024-02-24 15:28:18 +0200

    changed reply in /del

--- src/commands/delete_from_stats.ts
+++ src/commands/delete_from_stats.ts
@@ -4,7 +4,7 @@ import IActive from "../data/active";
 
 async function delete_from_stats(ctx: MyContext, active: YAMLWrapper<IActive>) {
     const chatMember = await ctx.getChatMember(ctx.from?.id || -1)
-    if (!chatMember || chatMember["status"] != 'creator') {
+    if (!chatMember || chatMember.status != 'creator') {
         ctx.reply("❌ Щось пішло не так або ви не є власником чату");
         return
     } else {
@@ -14,12 +14,14 @@ async function delete_from_stats(ctx: MyContext, active: YAMLWrapper<IActive>) {
             const name = active.data[ctx.chat.id][ctx.message.reply_to_message.from.id].name
             //@ts-expect-error
             delete active.data[ctx.chat.id][ctx.message.reply_to_message.from.id]
-            ctx.reply(`✅ Успішно видалено ${name} зі статистики.`)
+            ctx.reply(`✅ Успішно видалено ${name} з активу та приховано зі статистики.`)
         } else {
-            ctx.reply("❌ Цього користувача немає в статистиці.")
+            ctx.reply("❌ Цього користувача немає в активі.")
         }
     }
 
 }
 
 export default delete_from_stats;
+
+
