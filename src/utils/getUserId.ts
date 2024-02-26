4a5917b2be73f312ba0652d78acc2b57373afec7
Vlad Pavlenko <pavlenkoliar@gmail.com>
2024-02-26 05:44:30 +0200

    did some cringe

--- src/utils/getUserId.ts
+++ src/utils/getUserId.ts
@@ -3,10 +3,11 @@ import IActive from "../data/active";
 
 //** Returns user_id or -1 on fail*/
 function getUserId(
-  wantedUser: string,
+  wantedUser: string | undefined,
   chat_id: number | string,
   active: YAMLWrapper<IActive>
 ) {
+  if (wantedUser === undefined) return -1;
   if (wantedUser.startsWith("@")) {
     wantedUser = wantedUser.slice(1);
     for (const user in active.data[chat_id]) {
