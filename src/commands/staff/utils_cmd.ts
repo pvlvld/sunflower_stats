412a1e734f682193d10068cea139fb4b327e8742
Vlad Pavlenko <pavlenkoliar@gmail.com>
2024-02-29 16:26:27 +0200

    add chat log

--- src/commands/staff/utils_cmd.ts
+++ src/commands/staff/utils_cmd.ts
@@ -1,12 +1,20 @@
 import YAMLWrapper from "../../data/YAMLWrapper";
 import IActive from "../../data/active";
+import { MyContext } from "../../types/context";
 
-export function removeAnonimousActive(active: YAMLWrapper<IActive>) {
+export function removeAnonimousActive(
+  ctx: MyContext,
+  active: YAMLWrapper<IActive>
+) {
+  let hidedCount = 0;
   for (const chat_id in active.data) {
     for (const user_id in active.data[chat_id]) {
       if ([136817688, 777000].includes(+user_id)) {
         delete active.data[chat_id]?.[user_id];
+        hidedCount++;
       }
     }
   }
+
+  ctx.reply(`Done! Hided ${hidedCount} anonimous members`).catch(() => {});
 }
