773557b849bca2b3cce9d60b1f68de1e0660a7e4
Vlad Pavlenko <pavlenkoliar@gmail.com>
2024-03-07 22:09:51 +0200

    wrap sending in try-catch

--- src/commands/staff/broadcast_owners.ts
+++ src/commands/staff/broadcast_owners.ts
@@ -2,9 +2,9 @@ import moment from "moment";
 import IActive from "../../data/active";
 import YAMLWrapper from "../../data/YAMLWrapper";
 import { autoRetry } from "@grammyjs/auto-retry";
-import { GrammyError, type HearsContext } from "grammy";
-import type { ChatMemberOwner } from "@grammyjs/types";
+import type { HearsContext } from "grammy";
 import type { MyContext } from "../../types/context";
+import type { ChatMemberOwner } from "@grammyjs/types";
 
 async function broadcast_owners_cmd(
   ctx: HearsContext<MyContext>,
@@ -22,45 +22,28 @@ async function broadcast_owners_cmd(
   let owner: ChatMemberOwner | undefined;
   let counter = 0;
 
-  // REMOVE
-  let start_owner = 814121095;
-  let start_chat = "-1001898242958";
-  let blocked_owner = true;
-  let blocked_chat = true;
-  //REMOVE
-
   for (let chat in active.data) {
-    //REMOVE
-    if (chat === start_chat) {
-      blocked_chat = false;
-    }
-    if (blocked_chat) {
-      continue;
-    }
-    //REMOVE
     for (let user in active.data[chat]) {
       if (moment().diff(moment(active.data[chat]![user]!.active_last), "days") < 3) {
         admins = await ctx.api.getChatAdministrators(chat);
         owner = admins.filter((a) => a.status === "creator")[0] as any;
-        // REMOVE
-        if (owner) {
-          if ((owner.user.id = start_owner)) {
-            blocked_owner = false;
-            continue;
-          }
-          if (blocked_owner) {
-            continue;
-          }
-          // REMOVE
-
-          await ctx.api
-            .forwardMessage(owner.user.id, ctx.chat.id, ctx.msg.reply_to_message.message_id, {
-              disable_notification: true,
-            })
-            .catch((e) => {});
-          counter++;
+        if (!owner) {
+          continue;
         }
 
+        try {
+          await ctx.api.forwardMessage(
+            owner.user.id,
+            ctx.chat.id,
+            ctx.msg.reply_to_message.message_id,
+            {
+              disable_notification: true,
+            }
+          );
+          counter++;
+        } catch (e) {
+          console.error(e);
+        }
         break;
       }
     }
