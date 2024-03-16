839a9d0e4c318265781bbaa27854de6e777bce68
Vlad Pavlenko <pavlenkoliar@gmail.com>
2024-03-16 19:56:17 +0200

    update types

--- src/types/context.ts
+++ src/types/context.ts
@@ -1,8 +1,8 @@
-import type { ChatTypeContext, Filter } from "grammy";
+import type { ChatTypeContext, CommandContext, HearsContext } from "grammy";
 import type { Context } from "grammy";
 
 export type MyContext = Context;
 
 export type MyGroupContext = ChatTypeContext<MyContext, "group" | "supergroup">;
 
-export type GroupTextContext = Filter<MyGroupContext, ":text" | "edited_message:text" | ":caption">;
+export type GroupTextContext = CommandContext<MyGroupContext> | HearsContext<MyGroupContext>;
