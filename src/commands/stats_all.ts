f7a79ce3f74f2421d2a1fb0f56dee28db19caa37
Vlad Pavlenko <pavlenkoliar@gmail.com>
2024-03-18 23:40:19 +0200

    use htrime & big.js for performance logs

--- src/commands/stats_all.ts
+++ src/commands/stats_all.ts
@@ -6,6 +6,7 @@ import type TodayStats from "../data/stats";
 import type { MyContext } from "../types/context";
 import type YAMLWrapper from "../data/YAMLWrapper";
 import type { ChatTypeContext, HearsContext } from "grammy";
+const Big = require("big-js");
 
 async function stats_all(
   ctx: HearsContext<ChatTypeContext<MyContext, "supergroup" | "group">>,
@@ -13,18 +14,18 @@ async function stats_all(
   todayStats: TodayStats,
   active: YAMLWrapper<IActive>
 ) {
-  const start = Date.now();
+  const start = String(process.hrtime.bigint());
   const stats = await dbStats.chat.all(ctx.chat.id);
   if (!isDbResNotEmpty(stats)) {
     await ctx.reply("Щось пішло не так.");
     return;
   }
 
-  const queryTime = Date.now();
+  const queryTime = String(process.hrtime.bigint());
   const msg =
     "📊 Статистика чату за весь час:\n\n" +
     getStatsRatingPlusToday(stats, ctx.chat.id, todayStats, active);
-  const msgTime = Date.now();
+  const msgTime = String(process.hrtime.bigint());
 
   await ctx.reply(msg, {
     disable_notification: true,
@@ -33,7 +34,9 @@ async function stats_all(
 
   if (ctx.chat.id === -1001898242958) {
     ctx.reply(
-      `DB: ${queryTime - start}ms\nGen: ${msgTime - queryTime}ms\nTotal: ${msgTime - start}ms`
+      `DB: ${new Big(queryTime).minus(start).div(1000000)}ms\nGen: ${new Big(msgTime)
+        .minus(queryTime)
+        .div(1000000)}ms\nTotal: ${new Big(msgTime).minus(start).div(1000000)}ms`
     );
   }
 }
