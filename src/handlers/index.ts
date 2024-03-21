ec798f9176a3b85e2353d71450b9a425b1058483
Vlad Pavlenko <pavlenkoliar@gmail.com>
2024-03-21 14:18:35 +0200

    move left_statuses checks to index

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -4,13 +4,17 @@ import leaveChatMemberHandler from "./leaveChatMember";
 import type IActive from "../data/active";
 import type YAMLWrapper from "../data/YAMLWrapper";
 
+const left_statuses = ["kicked", "left"];
+
 function regHandlers(active: YAMLWrapper<IActive>) {
   bot.on("msg:new_chat_members:me", () => {
     botStatsManager.newGroup();
   });
 
   bot.on("chat_member", (ctx) => {
-    leaveChatMemberHandler(ctx, active);
+    if (left_statuses.includes(ctx.chatMember.new_chat_member.status)) {
+      leaveChatMemberHandler(ctx, active);
+    }
   });
 }
 
--- src/handlers/leaveChatMember.ts
+++ src/handlers/leaveChatMember.ts
@@ -3,15 +3,11 @@ import type IActive from "../data/active";
 import type { MyContext } from "../types/context";
 import type YAMLWrapper from "../data/YAMLWrapper";
 
-const left_statuses = ["kicked", "left"];
-
 function leaveChatMemberHandler(
   ctx: Filter<MyContext, "chat_member">,
   active: YAMLWrapper<IActive>
 ) {
-  if (left_statuses.includes(ctx.chatMember.new_chat_member.status)) {
-    delete active.data[ctx.chat.id]?.[ctx.chatMember.new_chat_member.user.id];
-  }
+  delete active.data[ctx.chat.id]?.[ctx.chatMember.new_chat_member.user.id];
 }
 
 export default leaveChatMemberHandler;
