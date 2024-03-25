0073fb76f0d034cf1ace0c1e7085c5f2b7aa878e
Vlad Pavlenko <pavlenkoliar@gmail.com>
2024-03-25 22:03:09 +0200

    add leave log

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -7,11 +7,14 @@ import type YAMLWrapper from "../data/YAMLWrapper";
 const left_statuses = ["kicked", "left"];
 
 function regHandlers(active: YAMLWrapper<IActive>) {
-  bot.on("my_chat_member", (ctx) => {
+  bot.on("my_chat_member", async (ctx) => {
     if (left_statuses.includes(ctx.myChatMember.old_chat_member.status)) {
       botStatsManager.joinGroup();
     } else if (left_statuses.includes(ctx.myChatMember.new_chat_member.status)) {
       botStatsManager.leftGroup();
+      if (ctx.chat.type === "supergroup" && ctx.chat.username) {
+        await ctx.api.sendMessage(-1004189530299, `#Left @${ctx.chat.username}`).catch((e) => {});
+      }
     }
   });
 
