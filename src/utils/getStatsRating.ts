d709182c04d247dd1288ba5c531a84a3ca7240e3
Vlad Pavlenko <pavlenkoliar@gmail.com>
2024-04-29 19:18:41 +0300

    refactor getStatsRatingPlusToday

--- src/utils/getStatsRating.ts
+++ src/utils/getStatsRating.ts
@@ -6,37 +6,26 @@ export function getStatsRatingPlusToday(stats: IDbChatUserStats[], chat_id: numb
   const replyParts: string[] = [];
   let totalChatMessages = 0;
 
-  const merged: { [user_id: string]: IDbChatUserStats } = {};
-  for (const item of stats) {
-    merged[item.user_id] = item;
-  }
-
-  let user_id: string;
-
-  const usersId_sorted = Object.keys(merged).sort((u1, u2) => {
-    return merged[u1].count < merged[u2].count ? 1 : -1;
-  });
-
-  const statsRowLimit = Math.min(50, usersId_sorted.length);
+  const statsRowLimit = Math.min(50, stats.length);
   let statsRowsCount = 1;
 
-  for (let i = 0; i < usersId_sorted.length + 1; i++) {
-    user_id = usersId_sorted[i];
-    if (active.data[chat_id]?.[user_id] && statsRowsCount < statsRowLimit + 1) {
+  let user: IDbChatUserStats;
+  for (user of stats) {
+    if (active.data[chat_id]?.[user.user_id] && statsRowsCount < statsRowLimit + 1) {
       replyParts.push(
         `${statsRowsCount}. ${getUserNameLink.html(
-          active.data[chat_id]?.[user_id]?.nickname ||
-            active.data[chat_id]?.[user_id]?.name ||
+          active.data[chat_id]?.[user.user_id]?.nickname ||
+            active.data[chat_id]?.[user.user_id]?.name ||
             "Невідомо",
-          active.data[chat_id]?.[user_id]?.username,
-          user_id
-        )} — ${merged[user_id].count}\n`
+          active.data[chat_id]?.[user.user_id]?.username,
+          user.user_id
+        )} — ${user.count || 0}\n`
       );
       statsRowsCount++;
     }
-
-    totalChatMessages += merged[user_id]?.count || 0;
+    totalChatMessages += user.count || 0;
   }
+
   replyParts.push(`\nЗагальна кількість повідомлень: ${totalChatMessages.toLocaleString("fr-FR")}`);
 
   return replyParts.join("");
