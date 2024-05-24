2c2c158e462526284d1efcc542d3b435c0bfe350
Vlad Pavlenko <pavlenkoliar@gmail.com>
2024-05-24 19:38:03 +0300

    add ChartCache

--- src/utils/cache.ts
+++ src/utils/cache.ts
@@ -4,6 +4,7 @@ import NodeCache from "node-cache";
 class CacheManager {
   public LRUCache: LRUCache<{}, {}, unknown>;
   public TTLCache: NodeCache;
+  public ChartCache: typeof ChartCache;
 
   constructor() {
     this.LRUCache = new LRUCache({
@@ -12,8 +13,24 @@ class CacheManager {
     });
 
     this.TTLCache = new NodeCache({ stdTTL: 60, checkperiod: 5 });
+
+    this.ChartCache = ChartCache;
   }
 }
+
+let _chartCache: Record<string, string | undefined | null> = {};
+const ChartCache = {
+  /** returns @string file_id on success, @undefined if no cached image @null if chart must not be sent*/
+  get: (id: number | string) => {
+    return _chartCache[id];
+  },
+  set: (id: number | string, file_id: string) => {
+    _chartCache[id] = file_id;
+  },
+  flush: () => {
+    _chartCache = {};
+  },
+};
 
 const cacheManager = new CacheManager();
 
