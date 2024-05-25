8df6e2e5d7dc9c66300246dc7683f1949373d020
Vlad Pavlenko <pavlenkoliar@gmail.com>
2024-05-25 20:51:00 +0300

    add chart img to stats their cmd

--- src/commands/stats_their.ts
+++ src/commands/stats_their.ts
@@ -1,7 +1,14 @@
 import getUserId from "../utils/getUserId";
 import getUserStatsMessage from "../utils/getUserStatsMessage";
 import type { IGroupTextContext } from "../types/context";
+import { getStatsChart } from "../chart/getStatsChart";
+import cacheManager from "../utils/cache";
 import dbStats from "../db/stats";
+import cfg from "../config";
+
+function getChartCacheKey(chat_id: number, user_id: number) {
+  return `${chat_id}_${user_id}`;
+}
 
 async function stats_their(ctx: IGroupTextContext) {
   const userId =
@@ -9,18 +16,49 @@ async function stats_their(ctx: IGroupTextContext) {
     getUserId((ctx.msg.text ?? ctx.msg.caption).slice(4), ctx.chat.id) ||
     -1;
 
-  if ([136817688, 777000, -1].includes(userId) || ctx.msg.reply_to_message?.from?.is_bot) {
-    await ctx.reply("Користувача не знайдено.");
-    return;
+  if (cfg.IGNORE_IDS.includes(ctx.from.id) || ctx.msg.reply_to_message?.from?.is_bot) {
+    void (await ctx.reply("Користувача не знайдено."));
   }
 
-  await ctx.reply(
-    getUserStatsMessage(ctx.chat.id, userId, await dbStats.user.all(ctx.chat.id, userId)),
-    {
-      disable_notification: true,
-      link_preview_options: { is_disabled: true },
+  const chartCacheKey = getChartCacheKey(ctx.chat.id, userId);
+  const cachedChart = cacheManager.ChartCache.get(chartCacheKey);
+
+  try {
+    switch (cachedChart.status) {
+      case "unrendered":
+        const chart = await getStatsChart(ctx.chat.id, userId);
+        if (chart) {
+          const msg = await ctx.replyWithPhoto(chart, {
+            caption: getUserStatsMessage(
+              ctx.chat.id,
+              userId,
+              await dbStats.user.all(ctx.chat.id, userId)
+            ),
+            disable_notification: true,
+          });
+          cacheManager.ChartCache.set(chartCacheKey, msg.photo[msg.photo.length - 1].file_id);
+        } else {
+          cacheManager.ChartCache.set(chartCacheKey, "");
+        }
+        return;
+      case "ok":
+        return void (await ctx.replyWithPhoto(cachedChart.file_id, {
+          caption: getUserStatsMessage(
+            ctx.chat.id,
+            userId,
+            await dbStats.user.all(ctx.chat.id, userId)
+          ),
+          disable_notification: true,
+        }));
+      case "skip":
+        return void (await ctx.reply(
+          getUserStatsMessage(ctx.chat.id, userId, await dbStats.user.all(ctx.chat.id, userId)),
+          { disable_notification: true, link_preview_options: { is_disabled: true } }
+        ));
     }
-  );
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 export default stats_their;
