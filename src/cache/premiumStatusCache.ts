b1c4237e244f01b6c60b9a4e606a274ba5cc4eca
Vlad Pavlenko <pavlenkoliar@gmail.com>
2024-05-26 23:20:56 +0300

    rewrite PremiumStatusCache to use object with premium status and cached status

--- src/cache/premiumStatusCache.ts
+++ src/cache/premiumStatusCache.ts
@@ -1,20 +1,34 @@
 import NodeCache from "node-cache";
 import { secondsUntilMidnight } from "../utils/secondsUntilMidnight";
 
+type IPremiumStatus = Readonly<{ status: boolean; cached: boolean }>;
+
 class PremiumStatusCache {
   private _premiumStatusCache: NodeCache;
+  private _uncachedStatus: IPremiumStatus;
 
   constructor() {
     this._premiumStatusCache = new NodeCache({ stdTTL: 0, checkperiod: 30 });
+    this._uncachedStatus = Object.freeze({ status: false, cached: false });
   }
 
   public set(id: number, status: boolean) {
     // TODO: connect with donate microservice, update cache on signals
-    this._premiumStatusCache.set(id, status, secondsUntilMidnight());
+    const premiumStatus: IPremiumStatus = Object.freeze({
+      status,
+      cached: true,
+    });
+    this._premiumStatusCache.set(id, premiumStatus, secondsUntilMidnight());
   }
-  get(id: number) {
-    return this._premiumStatusCache.get(id);
+
+  get(id: number): IPremiumStatus {
+    const premiumStatus = this._premiumStatusCache.get(id);
+    if (premiumStatus) {
+      return premiumStatus as IPremiumStatus;
+    }
+    return this._uncachedStatus;
   }
+
   del(id: number) {
     this._premiumStatusCache.del(id);
   }
