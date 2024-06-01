fbe145a41a8a177f53f04e555ba3343d08df6d28
Vlad Pavlenko <pavlenkoliar@gmail.com>
2024-06-01 21:20:11 +0300

    init sleep

---
+++ src/utils/sleep.ts
@@ -0,0 +1,10 @@
+function sleepSync(ms: number) {
+  const start = Date.now();
+  while (Date.now() - start < ms) {}
+}
+
+async function sleepAsync(ms: number) {
+  return await new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+export { sleepAsync, sleepSync };
