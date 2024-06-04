5680f729e278387cb8c945d3c7ffc2f37e0beac3
Vlad Pavlenko <pavlenkoliar@gmail.com>
2024-06-04 16:26:32 +0300

    feat: usechatbgforall setting, premium check for personal chart

--- src/chart/plugins/bgImagePlugin.ts
+++ src/chart/plugins/bgImagePlugin.ts
@@ -4,46 +4,12 @@ import { Chart } from "chart.js/auto";
 import { isPremium } from "../../utils/isPremium";
 import fs from "fs";
 import { IChartType } from "../getStatsChart";
+import cacheManager from "../../cache/cache";
+import { Database } from "../../db/db";
 
 const baseBgPath = "./data/chartBg";
 
 let defaultBg: Image;
-
-async function bgImagePlugin(chat_id: number, user_id: number, type: IChartType) {
-  let pluginBgImage: Image;
-  // const isChatPremium = await isPremium(chat_id);
-  // const isUserPremium = await isPremium(user_id);
-
-  if (type === "chat") {
-    if (fs.existsSync(`${baseBgPath}/${chat_id}.jpg`)) {
-      pluginBgImage = await loadImage(`${baseBgPath}/${chat_id}.jpg`);
-    } else {
-      pluginBgImage = await getDefaultBg();
-    }
-  } else {
-    // TODO:
-    // 1. premium chat global custom gb settings
-    // 2. premium user only
-    // (in that order)
-    if (fs.existsSync(`${baseBgPath}/${user_id}.jpg`)) {
-      pluginBgImage = await loadImage(`${baseBgPath}/${user_id}.jpg`);
-    } else {
-      pluginBgImage = await getDefaultBg();
-    }
-  }
-
-  return {
-    id: "customCanvasBackgroundImage",
-    beforeDraw: (chart: Chart) => {
-      if (pluginBgImage.complete) {
-        const ctx = chart.ctx;
-        ctx.drawImage(pluginBgImage, 0, 0);
-      } else {
-        pluginBgImage.onload = () => chart.draw();
-      }
-    },
-  };
-}
 
 async function getDefaultBg() {
   if (defaultBg) {
@@ -52,6 +18,59 @@ async function getDefaultBg() {
 
   defaultBg = await loadImage("data/chartBg/!default.jpg");
   return defaultBg;
+}
+
+async function loadBgImage(id: number): Promise<Image> {
+  const path = `${baseBgPath}/${id}.jpg`;
+  if (fs.existsSync(path)) {
+    return loadImage(path);
+  }
+
+  return getDefaultBg();
+}
+
+function createPlugin(image: Image) {
+  return {
+    id: "customCanvasBackgroundImage",
+    beforeDraw: (chart: Chart) => {
+      if (image.complete) {
+        const ctx = chart.ctx;
+        ctx.drawImage(image, 0, 0);
+      } else {
+        image.onload = () => chart.draw();
+      }
+    },
+  };
+}
+
+async function bgImagePlugin(chat_id: number, user_id: number, type: IChartType) {
+  let pluginBgImage: Image;
+
+  if (type === "chat") {
+    pluginBgImage = await loadBgImage(chat_id);
+  } else {
+    if (cacheManager.ChatSettingsCache.get(chat_id)?.usechatbgforall || false) {
+      if (await isPremium(chat_id)) {
+        pluginBgImage = await loadBgImage(chat_id);
+      } else {
+        pluginBgImage = await getDefaultBg();
+        Database.chatSettings.set(
+          chat_id,
+          cacheManager.ChatSettingsCache.set(chat_id, { usechatbgforall: false })
+        );
+      }
+      return createPlugin(pluginBgImage);
+    }
+
+    if (await isPremium(user_id)) {
+      pluginBgImage = await loadBgImage(user_id);
+      return createPlugin(pluginBgImage);
+    }
+
+    pluginBgImage = await getDefaultBg();
+  }
+
+  return createPlugin(pluginBgImage);
 }
 
 export { bgImagePlugin };
