3f41e59bbffee30f8bc1ab7d4a8a7e0938a4e655
Vlad Pavlenko <pavlenkoliar@gmail.com>
2024-06-04 18:39:01 +0300

    use sendSelfdestructMessage

--- src/commands/stats_chat_range.ts
+++ src/commands/stats_chat_range.ts
@@ -1,39 +1,55 @@
 import type { IGroupTextContext } from "../types/context";
-import { DBStats } from "../db/stats";
-import parseCmdArgs from "../utils/parseCmdArgs";
-import isValidNumbers from "../utils/isValidNumbers";
+import { sendSelfdestructMessage } from "../utils/sendSelfdestructMessage";
+import { getCachedOrDBChatSettings } from "../utils/chatSettingsUtils";
 import { getStatsRatingPlusToday } from "../utils/getStatsRating";
+import isValidNumbers from "../utils/isValidNumbers";
+import parseCmdArgs from "../utils/parseCmdArgs";
+import { DBStats } from "../db/stats";
 
 async function stats_chat_range_cmd(ctx: IGroupTextContext, validateDate = true) {
   const dateRange = parseCmdArgs(ctx.msg.text ?? ctx.msg.caption) as string[];
 
   if (dateRange.length > 2 || (validateDate && !isValidDateRange(dateRange))) {
-    return void (await ctx.reply(
-      'Команда має мати такий формат:\n"стата 2022.04.13" або стата "2022.04.13 2022.04.14"'
+    return void (await sendSelfdestructMessage(
+      ctx,
+      {
+        isChart: false,
+        text: 'Команда має мати такий формат:\n"стата 2022.04.13" або стата "2022.04.13 2022.04.14"',
+        chart: undefined,
+      },
+      true
     ));
   }
+  const chat_id = ctx.chat.id;
+  const chatSettings = await getCachedOrDBChatSettings(chat_id);
 
   if (dateRange.length === 2) {
-    return void (await ctx.reply(
-      `📊 Статистика чату за ${dateRange[0]} - ${dateRange[1]}:\n\n` +
-        getStatsRatingPlusToday(
-          await DBStats.chat.inRage(ctx.chat.id, [dateRange[0], dateRange[1]]),
-          ctx.chat.id
-        ),
+    return void (await sendSelfdestructMessage(
+      ctx,
       {
-        disable_notification: true,
-        link_preview_options: { is_disabled: true },
-      }
+        isChart: false,
+        text:
+          `📊 Статистика чату за ${dateRange[0]} - ${dateRange[1]}:\n\n` +
+          getStatsRatingPlusToday(
+            await DBStats.chat.inRage(chat_id, [dateRange[0], dateRange[1]]),
+            chat_id
+          ),
+        chart: undefined,
+      },
+      chatSettings.selfdestructstats
     ));
   }
 
-  return void (await ctx.reply(
-    `📊 Статистика чату за ${dateRange[0]}:\n\n` +
-      getStatsRatingPlusToday(await DBStats.chat.date(ctx.chat.id, dateRange[0]), ctx.chat.id),
+  return void (await sendSelfdestructMessage(
+    ctx,
     {
-      disable_notification: true,
-      link_preview_options: { is_disabled: true },
-    }
+      isChart: false,
+      text:
+        `📊 Статистика чату за ${dateRange[0]}:\n\n` +
+        getStatsRatingPlusToday(await DBStats.chat.date(chat_id, dateRange[0]), chat_id),
+      chart: undefined,
+    },
+    chatSettings.selfdestructstats
   ));
 }
 
