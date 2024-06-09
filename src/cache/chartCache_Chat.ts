f64634efe140299b6d4d99fe3762571b04ff1fd7
Vlad Pavlenko <pavlenkoliar@gmail.com>
2024-06-09 20:06:30 +0300

    add skip status

--- src/cache/chartCache_Chat.ts
+++ src/cache/chartCache_Chat.ts
@@ -1,14 +1,16 @@
-type IChartStatuses = "ok" | "unrendered";
-type IChartCache = { file_id: string; status: IChartStatuses };
+type IChartStatuses = "ok" | "skip" | "unrendered";
+type IChartCache = Readonly<{ file_id: string; status: IChartStatuses }>;
 type IDateRange = "weekRange" | "monthRange" | "yearRange" | "all";
 
 class ChartCache_Chat {
   private _chartCache: Map<number, Map<IDateRange, IChartCache>>;
   private _unrenderedChart: IChartCache;
+  private _skipChart: IChartCache;
 
   constructor() {
     this._chartCache = new Map();
     this._unrenderedChart = Object.freeze({ file_id: "", status: "unrendered" });
+    this._skipChart = Object.freeze({ file_id: "", status: "skip" });
   }
 
   public get(chat_id: number, range: IDateRange): IChartCache {
@@ -31,7 +33,7 @@ class ChartCache_Chat {
     }
 
     if (file_id.length === 0) {
-      void chatCache.set(range, this._unrenderedChart);
+      void chatCache.set(range, this._skipChart);
     } else {
       void chatCache.set(range, Object.freeze({ file_id, status: "ok" }));
     }
