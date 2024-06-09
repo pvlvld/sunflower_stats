4bc9ed7e56178239727aeb96b751f121a7f9ca0e
Vlad Pavlenko <pavlenkoliar@gmail.com>
2024-06-09 23:11:09 +0300

    add cancel button

--- src/ui/menus/leftGroup.ts
+++ src/ui/menus/leftGroup.ts
@@ -5,28 +5,33 @@ import cfg from "../../config";
 
 const leftGroup_menu = new Menu<IGroupContext>("leftGroup-menu", {
   autoAnswer: true,
-}).text("Видалити дані чату", async (ctx) => {
-  if (!cfg.ADMINS.includes(ctx.from.id)) {
-    return;
-  }
-  if (!ctx.msg?.text) {
-    return void (await ctx.answerCallbackQuery("Це меню застаріло, створіть нове."));
-  }
+})
+  .text("Видалити дані чату", async (ctx) => {
+    if (!cfg.ADMINS.includes(ctx.from.id)) {
+      return;
+    }
+    if (!ctx.msg?.text) {
+      return void (await ctx.answerCallbackQuery("Це меню застаріло, створіть нове."));
+    }
 
-  const chat_id = ctx.msg.text.substring(ctx.msg.text.lastIndexOf("-"));
-  const removedRows = await removeChatData(chat_id);
+    const chat_id = ctx.msg.text.substring(ctx.msg.text.lastIndexOf("-"));
+    const removedRows = await removeChatData(chat_id);
 
-  if (removedRows === -1) {
-    return void ctx
-      .editMessageText(ctx.msg.text + "\nНе вдалося видалити дані чату. Помилка бази даних.")
-      .catch((e) => {});
-  }
+    if (removedRows === -1) {
+      return void (await ctx
+        .editMessageText(ctx.msg.text + "\nНе вдалося видалити дані чату. Помилка бази даних.")
+        .catch((e) => {}));
+    }
 
-  return void ctx
-    .editMessageText(ctx.msg.text + `\nВидалено ${removedRows} записів.`, {
-      reply_markup: undefined,
-    })
-    .catch((e) => {});
-});
+    return void (await ctx
+      .editMessageText(ctx.msg.text + `\nВидалено ${removedRows} записів.`, {
+        reply_markup: undefined,
+      })
+      .catch((e) => {}));
+  })
+  .row()
+  .text("Відмінити", async (ctx) => {
+    void (await ctx.editMessageReplyMarkup().catch((e) => {}));
+  });
 
 export { leftGroup_menu };
