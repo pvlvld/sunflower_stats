419e280b026fc5c8fb3de44d6988740c51003a01
Vlad Pavlenko <pavlenkoliar@gmail.com>
2024-06-14 22:23:53 +0300

    extract user data as IActiveUser from IActive

--- src/data/active.ts
+++ src/data/active.ts
@@ -3,17 +3,17 @@ import YAMLWrapper from "./YAMLWrapper";
 export interface IActive {
   [chat_id: string | number]:
     | {
-        [user_id: string | number]:
-          | {
-              active_first: string;
-              active_last: string;
-              name: string;
-              nickname: string | null;
-              username: string | null;
-            }
-          | undefined;
+        [user_id: string | number]: IActiveUser | undefined;
       }
     | undefined;
+}
+
+export interface IActiveUser {
+  active_first: string;
+  active_last: string;
+  name: string;
+  nickname: string | null;
+  username: string | null;
 }
 
 export const active = new YAMLWrapper<IActive>(() => "active", "data/active");
