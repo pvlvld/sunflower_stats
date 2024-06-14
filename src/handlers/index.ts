5c526fa7ccc9fe3397f961587b0b85959678acb5
Vlad Pavlenko <pavlenkoliar@gmail.com>
2024-06-14 22:28:54 +0300

    feat: autofix first_seen date on member chat rejoin

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -46,6 +46,8 @@ function regHandlers() {
   bot.on("chat_member", (ctx) => {
     if (cfg.STATUSES.LEFT_STATUSES.includes(ctx.chatMember.new_chat_member.status)) {
       leaveChatMemberHandler(ctx);
+    } else if (cfg.STATUSES.LEFT_STATUSES.includes(ctx.chatMember.old_chat_member.status)) {
+      joinChatMember(ctx);
     }
     adminUpdateHandler(ctx);
   });
