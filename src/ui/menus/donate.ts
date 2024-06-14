cc5e56ec8019ac72764af4d0b84a3de1348f27b4
Vlad Pavlenko <pavlenkoliar@gmail.com>
2024-06-14 20:07:30 +0300

    fix user donate url

--- src/ui/menus/donate.ts
+++ src/ui/menus/donate.ts
@@ -7,7 +7,7 @@ const donate_menu = new Menu("donate-menu").dynamic(async (ctx, range) => {
 
 function getDonateUrl(ctx: Context): string {
   if (ctx.chat?.type === "private") {
-    return `https://send.monobank.ua/jar/6TjRWExdMt?a=15&t=user_id${ctx.chat!.id}`;
+    return `https://send.monobank.ua/jar/6TjRWExdMt?a=15&t=user_id${ctx.from!.id}`;
   } else {
     return `https://send.monobank.ua/jar/6TjRWExdMt?a=30&t=group_id${ctx.chat!.id}`;
   }
