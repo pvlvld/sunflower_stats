ba3083e70cc0bf4d55ec174390fc6303bbd4a61d
Vlad Pavlenko <pavlenkoliar@gmail.com>
2024-06-15 01:29:21 +0300

    init setUserJoinDate cmd

---
+++ src/commands/staff/setUserJoinDate.ts
@@ -0,0 +1,37 @@
+import type { IGroupTextContext } from "../../types/context";
+import { isValidDateOrDateRange } from "../../utils/isValidDateOrDateRange";
+import parseCmdArgs from "../../utils/parseCmdArgs";
+import isChatOwner from "../../utils/isChatOwner";
+import getUserId from "../../utils/getUserId";
+import { active } from "../../data/active";
+
+async function setUserJoinDate_cmd(ctx: IGroupTextContext) {
+  let date = (parseCmdArgs(ctx.msg.text ?? ctx.msg.caption) as string[])[1];
+  const chat_id = ctx.chat.id;
+  const target_id =
+    ctx.msg.reply_to_message?.from?.id ||
+    getUserId((ctx.msg.text ?? ctx.msg.caption).slice(13), chat_id) ||
+    -1;
+
+  if (!date || isValidDateOrDateRange([date])) {
+    return void (await ctx
+      .reply("Щось пішло не так.\nПриклад використання команди !дата вступу 2024.01.01")
+      .catch((e) => {}));
+  }
+
+  if (!(await isChatOwner(chat_id, ctx.from.id))) {
+    return void (await ctx.reply("Ця команда доступна лише власнику чату!"));
+  }
+
+  if (!active.data[chat_id]![target_id]) {
+    return void (await ctx.reply("Користувача не знайдено."));
+  }
+
+  active.data[chat_id]![target_id]!.active_first = date.split(".").join("-");
+
+  return void (await ctx.reply(
+    `Успішно змінено дату першої появи в чаті ${active.data[chat_id]![target_id]?.name}!`
+  ));
+}
+
+export { setUserJoinDate_cmd };
