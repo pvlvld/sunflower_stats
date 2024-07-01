f07cba440238b779c257a2a8df7acc11a89a7aa3
Vlad Pavlenko <pavlenkoliar@gmail.com>
2024-07-01 20:31:02 +0300

    use getChatUsernameOrInvite

--- src/commands/getChatInvite.ts
+++ src/commands/getChatInvite.ts
@@ -1,24 +1,27 @@
 import parseCmdArgs from "../utils/parseCmdArgs";
 import type { IContext } from "../types/context";
+import { getChatUsernameOrInvite } from "../utils/getChatUsernameOrInviteLink";
 
 async function getChatInvite_cmd(ctx: IContext) {
-  const chat_id = parseCmdArgs(ctx.msg?.text || "")?.[0];
-  if (chat_id) {
-    let reply = "";
-    try {
-      const chat_info = await ctx.api.getChat(chat_id);
-      if (chat_info.type === "private") return;
-      reply += `${chat_info.title}\n@${"username" in chat_info ? chat_info.username : "-"}\n${
-        chat_info.invite_link
-      }`;
-      await ctx.reply(reply, {
-        disable_notification: true,
-        link_preview_options: { is_disabled: true },
-      });
-    } catch (e) {
-      await ctx.reply("Щось пішло не так.");
-      console.error(e);
-    }
+  const chat_id = Number(parseCmdArgs(ctx.msg?.text || "")?.[0]);
+  if (!chat_id) {
+    return;
+  }
+
+  let reply = "";
+  try {
+    const chat_info = await getChatUsernameOrInvite(chat_id);
+    if (chat_info.type === "error") return;
+    reply += `${chat_info.rawChatInfo.title}\n@${
+      chat_info.type === "username" ? chat_info.username : chat_info.invite
+    }`;
+    await ctx.reply(reply, {
+      disable_notification: true,
+      link_preview_options: { is_disabled: true },
+    });
+  } catch (e) {
+    await ctx.reply("Щось пішло не так.");
+    console.error(e);
   }
 }
 
