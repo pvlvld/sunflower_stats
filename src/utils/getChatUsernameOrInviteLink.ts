f354ca7457d82ce1ad911ecf62d6f86c20a06591
Vlad Pavlenko <pavlenkoliar@gmail.com>
2024-07-01 20:26:39 +0300

    init getChatUsernameOrInvite

---
+++ src/utils/getChatUsernameOrInviteLink.ts
@@ -0,0 +1,24 @@
+import type { ChatFullInfo } from "@grammyjs/types";
+import bot from "../bot";
+
+type IResult =
+  | { type: "error" }
+  | { type: "username"; username: string; rawChatInfo: ChatFullInfo }
+  | { type: "invite"; invite: string; rawChatInfo: ChatFullInfo };
+
+async function getChatUsernameOrInvite(chat_id: number): Promise<IResult> {
+  const chat_info = await bot.api.getChat(chat_id);
+  if (chat_info.username) {
+    return { type: "username", username: chat_info.username, rawChatInfo: chat_info };
+  } else if (chat_info.invite_link) {
+    return {
+      type: "invite",
+      invite: chat_info.invite_link,
+      rawChatInfo: chat_info,
+    };
+  } else {
+    return { type: "error" };
+  }
+}
+
+export { getChatUsernameOrInvite };
