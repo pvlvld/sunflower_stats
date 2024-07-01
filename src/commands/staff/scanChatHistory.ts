f6e59333c7c8880c464370a349d776a002f1a6c5
Vlad Pavlenko <pavlenkoliar@gmail.com>
2024-07-02 00:20:17 +0300

    remove rendered charts on successful scan

--- src/commands/staff/scanChatHistory.ts
+++ src/commands/staff/scanChatHistory.ts
@@ -1,6 +1,7 @@
 import type { IGroupHearsContext } from "../../types/context";
 import { historyScanner } from "../../scanner/historyScanner";
 import { getChatUsernameOrInvite } from "../../utils/getChatUsernameOrInviteLink";
+import cacheManager from "../../cache/cache";
 
 async function scanChatHistory_cmd(ctx: IGroupHearsContext) {
   let chatIdentifier: string | number | undefined = ctx.msg.text?.split(" ")[1];
@@ -27,6 +28,8 @@ async function scanChatHistory_cmd(ctx: IGroupHearsContext) {
 Щось пішло не так під час сканування ${chatIdentifier}
 Відскановано: ${result.count} повідомлень.`);
   } else {
+    cacheManager.ChartCache_Chat.removeChat(result.chat_id);
+    cacheManager.ChartCache_User.removeChat(result.chat_id);
     return ctx.reply(`
 Успішно відскановано ${result.count} повідомлень в ${chatIdentifier}
 `);
