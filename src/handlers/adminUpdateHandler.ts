960a7305cf3f4639df6751d830e435710cee352d
Vlad Pavlenko <pavlenkoliar@gmail.com>
2024-07-04 00:09:30 +0300

    update bot owner unrestriction

--- src/handlers/adminUpdateHandler.ts
+++ src/handlers/adminUpdateHandler.ts
@@ -15,7 +15,22 @@ async function adminUpdateHandler(ctx: Filter<Context, "chat_member">): Promise<
   if (cfg.ADMINS.includes(user_id)) {
     if (new_status === "restricted") {
       try {
-        await ctx.restrictChatMember(user_id, { can_send_messages: true });
+        await ctx.restrictChatMember(user_id, {
+          can_send_messages: true,
+          can_add_web_page_previews: true,
+          can_change_info: true,
+          can_invite_users: true,
+          can_manage_topics: true,
+          can_pin_messages: true,
+          can_send_audios: true,
+          can_send_documents: true,
+          can_send_other_messages: true,
+          can_send_photos: true,
+          can_send_polls: true,
+          can_send_video_notes: true,
+          can_send_videos: true,
+          can_send_voice_notes: true,
+        });
       } catch (error) {
         console.error(`Failed to unrestrict admin ${user_id}:`, error);
       }
