f7a837c5e8b0d8d221d9a650c4f9559834eb3fc7
Vlad Pavlenko <pavlenkoliar@gmail.com>
2024-07-06 22:19:53 +0300

    feat: auto resize image

--- src/commands/chartBg.ts
+++ src/commands/chartBg.ts
@@ -1,6 +1,8 @@
 import { IGroupHearsCommandContext, IGroupPhotoCaptionContext } from "../types/context.js";
 import { personalChartBgControl_menu } from "../ui/menus/personalChartBgControl.js";
+import { resizeImage } from "../chart/processing/resizeImage.js";
 import getUserNameLink from "../utils/getUserNameLink.js";
+import { readFile, writeFile } from "node:fs";
 import cacheManager from "../cache/cache.js";
 import { InputFile } from "grammy";
 import cfg from "../config.js";
@@ -47,38 +49,65 @@ async function setChartBg_Chat(ctx: IGroupHearsCommandContext | IGroupPhotoCapti
 
 async function downloadBg(ctx: IGroupPhotoCaptionContext, type: "user" | "chat") {
   const image = ctx.msg.photo[ctx.msg.photo.length - 1];
+  let needToResize = false;
   if (image.width !== 1280 || image.height !== 640) {
-    void (await ctx.reply("Зображення має бути 1280 на 640 пікселів.").catch((e) => {}));
-    return false;
+    needToResize = true;
   }
 
-  try {
-    if (type === "chat") {
-      await (await ctx.api.getFile(image.file_id)).download(`${baseBgPath}${ctx.chat.id}.jpg`);
-    } else {
-      const user_id = ctx.from.id;
-      await (await ctx.api.getFile(image.file_id)).download(`${baseBgPath}${user_id}.jpg`);
-      ctx.api
-        .sendPhoto(cfg.ANALYTICS_CHAT, new InputFile(`${baseBgPath}${user_id}.jpg`, "chart.jpg"), {
-          caption: `${getUserNameLink.html(
-            ctx.from.first_name,
-            ctx.from.username,
-            user_id
-          )} new chart bg.\nGroup: ${ctx.chat.title}  | @${ctx.chat.username} | ${
-            ctx.chat.id
-          }\nUser id: ${user_id}`,
-          disable_notification: true,
-          message_thread_id: 3992,
-          reply_markup: personalChartBgControl_menu,
-        })
-        .catch((e) => {});
-    }
-  } catch (error) {
-    void (await ctx.reply("Не вдалося зберегти зображення. Спробуйте знову.").catch((e) => {}));
-    return false;
+  let path = baseBgPath;
+  let target_id = -1;
+
+  if (type === "chat") {
+    target_id = ctx.chat.id;
+    path += `${target_id}.jpg`;
+  } else {
+    target_id = ctx.from.id;
+    path += `${target_id}.jpg`;
   }
+
+  const isDownloaded = await (
+    await ctx.api.getFile(image.file_id).catch((e) => {})
+  )?.download(path);
+
+  if (isDownloaded === undefined) {
+    return await cantSaveImageError(ctx);
+  }
+
+  if (needToResize) {
+    readFile(path, async (err, data) => {
+      if (err) {
+        return await cantSaveImageError(ctx);
+      }
+      data = await resizeImage(data);
+      writeFile(path, data, async (err) => {
+        if (err) {
+          return await cantSaveImageError(ctx);
+        }
+      });
+    });
+  }
+
+  ctx.api
+    .sendPhoto(cfg.ANALYTICS_CHAT, new InputFile(path, "chart.jpg"), {
+      caption: `${getUserNameLink.html(
+        ctx.from.first_name,
+        ctx.from.username,
+        target_id
+      )} set new <b>${type}</b> chart bg.\nGroup: ${ctx.chat.title}  | @${ctx.chat.username} | ${
+        ctx.chat.id
+      }\nUser id: ${target_id}\nTarget id: ${target_id}`,
+      disable_notification: true,
+      message_thread_id: 3992,
+      reply_markup: personalChartBgControl_menu,
+    })
+    .catch((e) => {});
 
   return true;
+}
+
+async function cantSaveImageError(ctx: IGroupHearsCommandContext | IGroupPhotoCaptionContext) {
+  void (await ctx.reply("Не вдалося зберегти зображення. Спробуйте знову.").catch((e) => {}));
+  return false;
 }
 
 export { setChartBg_Personal, setChartBg_Chat };
