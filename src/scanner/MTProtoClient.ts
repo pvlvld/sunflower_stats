8642ace852bc551cffa8b79566166c7b52d8c41a
Vlad Pavlenko <pavlenkoliar@gmail.com>
2024-07-11 17:46:58 +0300

    add getChat method

--- src/scanner/MTProtoClient.ts
+++ src/scanner/MTProtoClient.ts
@@ -14,6 +14,25 @@ class MTProtoClient {
       console.log(`Scanner: Logged in as ${self.displayName}`);
     });
   }
+
+  public async getChat(id: number | string) {
+    let chat: Chat | undefined;
+    let errorMessage: string = "";
+
+    try {
+      chat = await this._client.getChat(id);
+    } catch (e) {
+      if ((e as Error).message?.includes("haven't joined")) {
+        errorMessage = "Сканер не зміг отримати інформацію про чат. Можливо він заблокований.";
+      } else {
+        errorMessage = (e as Error).message;
+      }
+
+      return { status: false, chat: undefined, errorMessage } as const;
+    }
+
+    return { status: true, chat: chat, errorMessage } as const;
+  }
 }
 
 export { MTProtoClient };
