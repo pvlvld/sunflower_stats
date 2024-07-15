31060650dfe4afa9db138f2731f0f91e1a64225a
Vlad Pavlenko <pavlenkoliar@gmail.com>
2024-07-16 02:24:35 +0300

    return default chat settings in case something went wrong

--- src/db/chatSettings.ts
+++ src/db/chatSettings.ts
@@ -1,3 +1,4 @@
+import { DefaultChatSettings } from "../cache/chatSettingsCache.js";
 import type { IChatSettings } from "../types/settings.js";
 import { IDBPoolManager } from "./poolManager.js";
 
@@ -9,11 +10,22 @@ class DbChatSettingWrapper {
   }
 
   public async get(chat_id: number) {
-    return (
-      await this._poolManager.getPoolRead.query(
-        `SELECT charts, statsadminsonly, usechatbgforall, selfdestructstats FROM chats WHERE chat_id = ${chat_id};`
-      )
-    ).rows[0] as IChatSettings | undefined;
+    let settings_db: undefined | IChatSettings;
+    try {
+      settings_db = (
+        await this._poolManager.getPoolRead.query(
+          `SELECT charts, statsadminsonly, usechatbgforall, selfdestructstats FROM chats WHERE chat_id = ${chat_id};`
+        )
+      ).rows[0] as IChatSettings | undefined;
+    } catch (e) {
+      console.error("Error fetching chat settings:", e);
+    }
+
+    if (!settings_db) {
+      return { ...DefaultChatSettings };
+    }
+
+    return { ...settings_db };
   }
 
   public async set(chat_id: number, settings: IChatSettings) {
