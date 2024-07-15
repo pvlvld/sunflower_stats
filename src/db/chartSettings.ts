c56dae75f55475f74c584166ebcf5df417d1266b
Vlad Pavlenko <pavlenkoliar@gmail.com>
2024-07-15 03:38:32 +0300

    add default settings & type

--- src/db/chartSettings.ts
+++ src/db/chartSettings.ts
@@ -1,7 +1,17 @@
 import { RGB } from "../utils/hexToRGB.js";
 
+const _defaultChartSettings = {
+  line_color: "#e9bd07",
+  font_color: "#eeeeee",
+};
+
+const DefaultChartSettings = Object.freeze(_defaultChartSettings);
+
+type IChartSettings = typeof _defaultChartSettings;
+
 class ChartSettings {
   constructor(public line_color: RGB, public font_color: RGB) {}
 }
 
 export default ChartSettings;
+export { DefaultChartSettings, IChartSettings };
