a03be42eb828c5081aca821f0d5e2422f6e8a73e
Vlad Pavlenko <pavlenkoliar@gmail.com>
2024-07-18 16:13:45 +0300

    feat: call history scan on chat join ot send proposal

--- src/handlers/updateChatBotStatus.ts
+++ src/handlers/updateChatBotStatus.ts
@@ -1,23 +1,48 @@
+import { historyScanProposal_menu } from "../ui/menus/historyScanProposal.js";
 import type { IGroupMyChatMemberContext } from "../types/context.js";
+import { historyScanner } from "../scanner/historyScanner.js";
 import { botStatsManager } from "../commands/botStats.js";
 import { leftGroup_menu } from "../ui/menus/leftGroup.js";
 import { sleepAsync } from "../utils/sleep.js";
 import { hello } from "../commands/hello.js";
+import formattedDate from "../utils/date.js";
 import cacheManager from "../cache/cache.js";
 import { active } from "../data/active.js";
 import help_cmd from "../commands/help.js";
+import { DBStats } from "../db/stats.js";
 import cfg from "../config.js";
 
 async function updateChatBotStatus_handler(ctx: IGroupMyChatMemberContext) {
   // Bot join chat
   if (cfg.STATUSES.LEFT_STATUSES.includes(ctx.myChatMember.old_chat_member.status)) {
+    const firstRecordedMessageDate = await DBStats.chat.firstRecordDate(ctx.chat.id);
+
+    // History scan only if bot was not in chat earlier.
+    if (
+      !firstRecordedMessageDate ||
+      formattedDate.dateToYYYYMMDD(firstRecordedMessageDate) === formattedDate.today[0]
+    ) {
+      if (ctx.chat.username) {
+        historyScanner.scanChat(ctx.chat.username, ctx.chat.id);
+      } else {
+        await ctx.reply(
+          "Відсканувати історію чату, щоб старі повідомлення відображались в статистиці чату?",
+          {
+            reply_markup: historyScanProposal_menu,
+            //@ts-expect-error
+            reply_parameters: { message_id: ctx.msg?.id ?? -1, allow_sending_without_reply: true },
+          }
+        );
+      }
+    }
+
     await hello(ctx);
     await help_cmd(ctx);
     botStatsManager.joinGroup();
 
     if (ctx.chat.type === "supergroup" || ctx.chat.type === "group") {
       try {
-        await sleepAsync(1000);
+        await sleepAsync(500);
         const membersCount = (await ctx.getChatMemberCount().catch((e) => {})) || 0;
         const invite = (await ctx.getChat().catch((e) => {}))?.invite_link || "-";
 
