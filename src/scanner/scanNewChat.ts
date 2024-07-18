0da96382331e88cffb7e9e6a4b595dc6d24398b9
Vlad Pavlenko <pavlenkoliar@gmail.com>
2024-07-18 14:49:40 +0300

    init scanNewChat wrapper around scanner

---
+++ src/scanner/scanNewChat.ts
@@ -0,0 +1,62 @@
+import { historyScanner } from "./historyScanner.js";
+import { IGroupContext } from "../types/context.js";
+import { GrammyError } from "grammy";
+
+async function scanNewChat(ctx: IGroupContext, automatic = true) {
+  let identifier = "";
+
+  if (ctx.chat.username) {
+    identifier = ctx.chat.username;
+  } else {
+    try {
+      const invite = await ctx.createChatInviteLink({
+        creates_join_request: false,
+        member_limit: 1,
+      });
+      identifier = invite.invite_link;
+    } catch (e) {
+      if (e instanceof GrammyError) {
+        if (e.description.includes("not enough rights")) {
+          void (await ctx
+            .reply(
+              "Видайте мені дозвіл запрошувати учасників або зробіть чат публічним, щоб я зміг додати сканер."
+            )
+            .catch((e) => {}));
+          return "not enough rights";
+        }
+      }
+      console.error(e);
+    }
+  }
+
+  if (identifier.length == 0) {
+    if (!automatic) {
+      void (await ctx
+        .reply(
+          "Не вдалось отримати ідентифікатор чату для додавання сканеру. Переконайтеся, що я маю право додавати нових учасників або зробіть чат публічним."
+        )
+        .catch((e) => {}));
+    }
+
+    return;
+  }
+  const result = await historyScanner.scanChat(identifier);
+
+  if (!result.status) {
+    const error = result.error instanceof Error ? result.error.message : result.error;
+    console.error(error);
+    if (!automatic) {
+      void (await ctx.reply(error).catch((e) => {}));
+    }
+    return;
+  } else {
+    if (!automatic) {
+      void (await ctx
+        .reply(`Успішно відскановано та додано в статистику ${result.count} повідомлень!`)
+        .catch((e) => {}));
+    }
+    return;
+  }
+}
+
+export { scanNewChat };
