c931bcf41fcfa96b5d9ba7b20b3ff13574ae779f
Vlad Pavlenko <pavlenkoliar@gmail.com>
2024-08-06 15:25:18 +0300

    rewrite html replacer

--- src/utils/escape.ts
+++ src/utils/escape.ts
@@ -12,8 +12,14 @@ function escapeMarkdownV1(str: string): string {
   return str.replace(/([_*`[])/g, "\\$1");
 }
 
-function escapeHTML(str: string): string {
-  return str.replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/&/g, "&amp");
+const html_replacements = Object.freeze({
+  "<": "&lt;",
+  ">": "&gt;",
+  "&": "&amp;",
+  '"': "&quot;",
+}) as Record<string, string>;
+function escapeHTML(text: string) {
+  return text.replace(/[<>&"]/g, (char) => html_replacements[char]);
 }
 
 export default Escape;
