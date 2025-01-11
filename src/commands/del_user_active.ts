737fb28b236b05d2ebf52112de5bf45d7235dbe8
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-01-11 21:51:23 +0200

    allow users remove their own active

--- src/commands/del_user_active.ts
+++ src/commands/del_user_active.ts
@@ -6,31 +6,26 @@ import cfg from "../config.js";
 
 async function del_user_active(ctx: IGroupHearsContext) {
     const chatMember = await ctx.getChatMember(ctx.from?.id || -1).catch(() => {});
-    if (chatMember?.status === "creator" || cfg.ADMINS.includes(ctx.from?.id || -1)) {
-        const chat_id = ctx.chat.id;
-        const userId =
-            ctx.msg.reply_to_message?.from?.id ||
-            getUserId((ctx.msg.text ?? ctx.msg.caption).slice(13), chat_id) ||
-            -1;
+    const isCanDelOthers = chatMember?.status === "creator" || cfg.ADMINS.includes(ctx.from?.id || -1);
+    const rawTarget = (ctx.msg.text ?? ctx.msg.caption).split(" ")[1];
 
-        if (userId !== -1 && active.data[chat_id]?.[userId]) {
-            const targetName = active.data[chat_id][userId].name as string;
-            delete active.data[chat_id][userId];
-            await ctx
-                .reply(
-                    `✅ Успішно видалено ${Escape.html(targetName)} з активу та приховано зі статистики.`,
-                    { parse_mode: "HTML" }
-                )
-                .catch((e) => console.error(e));
-            return;
-        } else {
-            await ctx.reply("❌ Користувача не знайдено").catch((e) => {});
-            return;
-        }
+    const chat_id = ctx.chat.id;
+    const userId = isCanDelOthers
+        ? ctx.msg.reply_to_message?.from?.id || getUserId(rawTarget, chat_id) || ctx.from.id
+        : ctx.from.id;
+
+    if (active.data[chat_id]?.[userId]) {
+        const targetName = active.data[chat_id][userId].name as string;
+        delete active.data[chat_id][userId];
+        await ctx
+            .reply(`✅ Успішно видалено ${Escape.html(targetName)} з активу та приховано зі статистики.`, {
+                parse_mode: "HTML",
+            })
+            .catch((e) => console.error(e));
+    } else {
+        await ctx.reply("❌ Користувача не знайдено").catch((e) => {});
     }
-
-    await ctx.reply("❌ Щось пішло не так або ви не є власником чату");
-    return;
+    ctx.deleteMessage().catch(() => {});
 }
 
 export default del_user_active;
