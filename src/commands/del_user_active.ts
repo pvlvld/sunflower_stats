bca6b7536acdcb94fe09e604abc7043d183ec245
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-01-11 22:25:11 +0200

    fix remove active

--- src/commands/del_user_active.ts
+++ src/commands/del_user_active.ts
@@ -7,14 +7,16 @@ import cfg from "../config.js";
 async function del_user_active(ctx: IGroupHearsContext) {
     const chatMember = await ctx.getChatMember(ctx.from?.id || -1).catch(() => {});
     const isCanDelOthers = chatMember?.status === "creator" || cfg.ADMINS.includes(ctx.from?.id || -1);
-    const rawTarget = (ctx.msg.text ?? ctx.msg.caption).split(" ")[1];
+    const rawTarget = (ctx.msg.text ?? ctx.msg.caption).split(" ").at(-1)!;
 
     const chat_id = ctx.chat.id;
     const userId = isCanDelOthers
-        ? ctx.msg.reply_to_message?.from?.id || (rawTarget ? getUserId(rawTarget, chat_id) : undefined) || ctx.from.id
+        ? ctx.msg.reply_to_message?.from?.id ||
+          (["вступ", "!ссприховати"].includes(rawTarget) ? undefined : getUserId(rawTarget, chat_id)) ||
+          ctx.from.id
         : ctx.from.id;
 
-        if (active.data[chat_id]?.[userId]) {
+    if (active.data[chat_id]?.[userId]) {
         const targetName = active.data[chat_id][userId].name as string;
         delete active.data[chat_id][userId];
         await ctx
