792e60b2899862cb54996a8a4c240d4a50c6bcdf
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-01-18 19:14:46 +0200

    start menu fix

--- src/ui/menus/start.ts
+++ src/ui/menus/start.ts
@@ -2,10 +2,10 @@ import help_cmd from "../../commands/help.js";
 import type { IContext } from "../../types/context.js";
 import { Menu } from "@grammyjs/menu";
 
-const start_menu = new Menu<IContext>("help-menu", {
+const start_menu = new Menu<IContext>("sart-menu", {
     autoAnswer: true,
 })
-    .text("Команди бота", async (ctx) => help_cmd(ctx))
+    .text("Команди бота", (ctx) => void help_cmd(ctx))
     .row()
     .url("Додати бота в чат", "https://t.me/soniashnyk_statistics_bot?startgroup")
     .row()
