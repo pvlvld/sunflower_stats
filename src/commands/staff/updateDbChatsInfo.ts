bda347e97e1fdcb814df00e52c797eacb7d4bb18
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-02-12 11:03:37 +0200

    handle errors

--- src/commands/staff/updateDbChatsInfo.ts
+++ src/commands/staff/updateDbChatsInfo.ts
@@ -2,21 +2,35 @@ import { autoRetry } from "@grammyjs/auto-retry";
 import { active } from "../../data/active.js";
 import { IGroupHearsContext } from "../../types/context.js";
 import { Database } from "../../db/db.js";
+import { GrammyError } from "grammy";
+import { chatMigrationHandler } from "../../handlers/chatMigrationHandler.js";
 
 async function updateDbChatsInfo(ctx: IGroupHearsContext) {
-    const total_count = Object.keys(active.data).length;
+    const chats = Object.keys(active.data);
+    const total_count = chats.length;
     let count = 0;
     ctx.api.config.use(autoRetry());
     const statusMessage = await ctx.reply(`Оновлення інформації про чати...\n\n0/${total_count}`).catch((e) => {});
-    for (const chat in active.data) {
-        await ctx.api.getChat(chat).then((chatInfo) => {
-            if (!chatInfo.title) return;
-            Database.poolManager.getPool.query(`INSERT INTO public.chats (chat_id, title)
-                VALUES (${chatInfo.id}, '${chatInfo.title}')
-                ON CONFLICT (chat_id)
-                DO UPDATE SET title = EXCLUDED.title;`);
+    for (const chat of chats) {
+        const chatInfo = await ctx.api.getChat(chat).catch((e) => {
+            if (!(e instanceof GrammyError)) return;
+            if (e.description.includes("bot was kicked")) {
+                delete active.data[chat];
+            }
+            if (e.description.includes("group chat was upgraded to a supergroup chat")) {
+                chatMigrationHandler.handleFromError(e);
+            }
+            if (e.description.includes("chat not found")) {
+                delete active.data[chat];
+            }
         });
         count++;
+        if (!chatInfo) return;
+        Database.poolManager.getPool.query(`INSERT INTO public.chats (chat_id, title)
+            VALUES (${chatInfo.id}, '${chatInfo.title}')
+            ON CONFLICT (chat_id)
+            DO UPDATE SET title = EXCLUDED.title;`);
+
         if (count % 100 !== 0) continue;
         if (statusMessage) {
             await ctx.api
