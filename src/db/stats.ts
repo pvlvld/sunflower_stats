742d3279847ac0997019b0dd9dbbb1261bc149c6
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-02-20 23:18:46 +0200

    refactor & utilize prepared queries

--- src/db/stats.ts
+++ src/db/stats.ts
@@ -4,6 +4,43 @@ import { DBPoolManager, IDBPoolManager } from "./poolManager.js";
 
 type IDateRanges = keyof IFormattedRangeDateGetters | [from: string, to: string];
 
+const queries = Object.freeze({
+    stats: {
+        user: {
+            personal: `SELECT
+                        SUM(count) AS total,
+                        SUM(CASE WHEN date BETWEEN $1 AND $2 THEN count ELSE 0 END) AS year,
+                        SUM(CASE WHEN date BETWEEN $3 AND $4 THEN count ELSE 0 END) AS month,
+                        SUM(CASE WHEN date BETWEEN $5 AND $6 THEN count ELSE 0 END) AS week,
+                        SUM(CASE WHEN date = $7 THEN count ELSE 0 END) AS today,
+                        TO_CHAR(MIN(date), 'YYYY-MM-DD') AS first_seen
+                        FROM stats_daily
+                        WHERE chat_id = $8 AND user_id = $9`,
+        },
+        chat: {
+            // weekRange: `SELECT 1`,
+            // monthRange: `SELECT 1`,
+            // yearRange: `SELECT 1`,
+            customRange: `SELECT user_id, SUM(count) AS count
+                            FROM stats_daily
+                            WHERE chat_id = $1 AND date BETWEEN $2 AND $3
+                            GROUP BY user_id
+                            ORDER BY count DESC`,
+            date: `SELECT user_id, SUM(count) AS count
+                    FROM stats_daily
+                    WHERE chat_id = $1 AND date = $2
+                    GROUP BY user_id
+                    ORDER BY count DESC`,
+            all: `SELECT user_id, SUM(count) AS count
+                    FROM stats_daily
+                    WHERE chat_id = $1
+                    GROUP BY user_id
+                    ORDER BY count DESC`,
+        },
+        global: {},
+    },
+});
+
 class DBStatsWrapper {
     public chat;
     public user;
@@ -24,20 +61,24 @@ class DBUserStats {
     }
 
     async all(chat_id: number, user_id: number): Promise<IDBChatUserStatsAll> {
-        const query = `
-    SELECT
-    SUM(count) AS total,
-    SUM(CASE WHEN date BETWEEN '${formattedDate.yearRange[0]}' AND '${formattedDate.yearRange[1]}' THEN count ELSE 0 END) AS year,
-    SUM(CASE WHEN date BETWEEN '${formattedDate.monthRange[0]}' AND '${formattedDate.monthRange[1]}' THEN count ELSE 0 END) AS month,
-    SUM(CASE WHEN date BETWEEN '${formattedDate.weekRange[0]}' AND '${formattedDate.weekRange[1]}' THEN count ELSE 0 END) AS week,
-    SUM(CASE WHEN date = '${formattedDate.today[0]}' THEN count ELSE 0 END) AS today,
-    TO_CHAR(MIN(date), 'YYYY-MM-DD') AS first_seen
-    FROM stats_daily
-    WHERE chat_id = ${chat_id} AND user_id = ${user_id};
-    `;
-
         try {
-            return (await this._dbPooolManager.getPoolRead.query(query)).rows[0] as IDBChatUserStatsAll;
+            return (
+                await this._dbPooolManager.getPoolRead.query({
+                    name: "get_stats_user_chat_personal",
+                    text: queries.stats.user.personal,
+                    values: [
+                        formattedDate.yearRange[0],
+                        formattedDate.yearRange[1],
+                        formattedDate.monthRange[0],
+                        formattedDate.monthRange[1],
+                        formattedDate.weekRange[0],
+                        formattedDate.weekRange[1],
+                        formattedDate.today[0],
+                        chat_id,
+                        user_id,
+                    ],
+                })
+            ).rows[0] as IDBChatUserStatsAll;
         } catch (error) {
             console.error(error);
             return {} as IDBChatUserStatsAll;
@@ -86,14 +127,13 @@ class DBChatStats {
 
     async today(chat_id: number): Promise<IDBChatUserStats[]> {
         try {
-            const query = `
-      SELECT user_id, SUM(count)  AS count
-      FROM stats_daily
-      WHERE chat_id = ${chat_id} AND date = '${formattedDate.today}'
-      GROUP BY user_id
-      ORDER BY count DESC;
-        `;
-            return (await this._dbPoolManager.getPoolRead.query(query)).rows as IDBChatUserStats[];
+            return (
+                await this._dbPoolManager.getPoolRead.query({
+                    name: "stats_chat_today",
+                    text: queries.stats.chat.date,
+                    values: [chat_id, formattedDate.today],
+                })
+            ).rows as IDBChatUserStats[];
         } catch (error) {
             console.error(error);
             return [] as IDBChatUserStats[];
@@ -102,14 +142,13 @@ class DBChatStats {
 
     async yesterday(chat_id: number): Promise<IDBChatUserStats[]> {
         try {
-            const query = `
-      SELECT user_id, SUM(count)  AS count
-      FROM stats_daily
-      WHERE chat_id = ${chat_id} AND date = '${formattedDate.yesterday[0]}'
-      GROUP BY user_id
-      ORDER BY count DESC;
-        `;
-            return (await this._dbPoolManager.getPoolRead.query(query)).rows as IDBChatUserStats[];
+            return (
+                await this._dbPoolManager.getPoolRead.query({
+                    name: "stats_chat_yesterday",
+                    text: queries.stats.chat.date,
+                    values: [chat_id, formattedDate.yesterday[0]],
+                })
+            ).rows as IDBChatUserStats[];
         } catch (error) {
             console.error(error);
             return [] as IDBChatUserStats[];
@@ -121,23 +160,19 @@ class DBChatStats {
             if (typeof rawRange === "string") {
                 const range = formattedDate[rawRange];
                 return (
-                    await this._dbPoolManager.getPoolRead.query(`
-        SELECT user_id, SUM(count) AS count
-        FROM stats_daily
-        WHERE chat_id = ${chat_id} AND date BETWEEN '${range[0]}' AND '${range[1]}'
-        GROUP BY user_id
-        ORDER BY count DESC;
-          `)
+                    await this._dbPoolManager.getPoolRead.query({
+                        name: "stats_chat_customrange",
+                        text: queries.stats.chat.customRange,
+                        values: [chat_id, range[0], range[1]],
+                    })
                 ).rows as IDBChatUserStats[];
             } else {
                 return (
-                    await this._dbPoolManager.getPoolRead.query(`
-        SELECT user_id, SUM(count) AS count
-        FROM stats_daily
-        WHERE chat_id = ${chat_id} AND date BETWEEN '${rawRange[0]}' AND '${rawRange[1]}'
-        GROUP BY user_id
-        ORDER BY count DESC;
-          `)
+                    await this._dbPoolManager.getPoolRead.query({
+                        name: "stats_chat_customrange",
+                        text: queries.stats.chat.customRange,
+                        values: [chat_id, rawRange[0], rawRange[1]],
+                    })
                 ).rows as IDBChatUserStats[];
             }
         } catch (error) {
@@ -149,13 +184,11 @@ class DBChatStats {
     async date(chat_id: number, date: string) {
         try {
             return (
-                await this._dbPoolManager.getPoolRead.query(`
-        SELECT user_id, SUM(count) AS count
-        FROM stats_daily
-        WHERE chat_id = ${chat_id} AND date = '${date}'
-        GROUP BY user_id
-        ORDER BY count DESC;
-          `)
+                await this._dbPoolManager.getPoolRead.query({
+                    name: "stats_chat_date",
+                    text: queries.stats.chat.date,
+                    values: [chat_id, date],
+                })
             ).rows as IDBChatUserStats[];
         } catch (error) {
             console.error(error);
@@ -164,14 +197,14 @@ class DBChatStats {
     }
 
     async all(chat_id: number): Promise<IDBChatUserStats[]> {
-        const query = `
-    SELECT user_id, SUM(count) AS count
-    FROM stats_daily
-    WHERE chat_id = ${chat_id}
-    GROUP BY user_id
-    ORDER BY count DESC;`;
         try {
-            return (await this._dbPoolManager.getPoolRead.query(query)).rows as IDBChatUserStats[];
+            return (
+                await this._dbPoolManager.getPoolRead.query({
+                    name: "stats_chat_all",
+                    text: queries.stats.chat.all,
+                    values: [chat_id],
+                })
+            ).rows as IDBChatUserStats[];
         } catch (error) {
             console.error(error);
             return [] as IDBChatUserStats[];
