0ccc892cfc96f7a419677f3b5f9a41d984421bab
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-02-21 01:35:13 +0200

    fix chat title update query

--- src/handlers/chatTitleUpdateHandler.ts
+++ src/handlers/chatTitleUpdateHandler.ts
@@ -3,8 +3,9 @@ import { Database } from "../db/db.js";
 import { IContext } from "../types/context.js";
 
 async function chatTitleUpdateHandler(ctx: Filter<IContext, ":new_chat_title">): Promise<void> {
-    await Database.poolManager.getPool.query(`UPDATE chats SET title = '$1' WHERE chat_id = ${ctx.chat.id};`, [
+    await Database.poolManager.getPool.query(`UPDATE chats SET title = $1 WHERE chat_id = $2;`, [
         ctx.msg.new_chat_title,
+        ctx.chat.id,
     ]);
 }
 
