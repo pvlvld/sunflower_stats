1227f37feee7bbb30a46451fe116776c5d67c96b
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-02-21 16:33:34 +0200

    fix: sanitize chat title updates

--- src/handlers/chatTitleUpdateHandler.ts
+++ src/handlers/chatTitleUpdateHandler.ts
@@ -1,10 +1,11 @@
 import { Filter } from "grammy";
 import { Database } from "../db/db.js";
 import { IContext } from "../types/context.js";
+import Escape from "../utils/escape.js";
 
 async function chatTitleUpdateHandler(ctx: Filter<IContext, ":new_chat_title">): Promise<void> {
     await Database.poolManager.getPool.query(`UPDATE chats SET title = $1 WHERE chat_id = $2;`, [
-        ctx.msg.new_chat_title,
+        Escape.html(ctx.msg.new_chat_title),
         ctx.chat.id,
     ]);
 }
