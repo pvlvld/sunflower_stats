42928ef0acb10333999b3e66b14a5a917279cbe0
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-03-01 20:36:21 +0200

    refactor: extract chart configuration logic into a new utility module

---
+++ src/chart/utils/getChartConfig.ts
@@ -0,0 +1,99 @@
+const getChartConfig = {
+    default: getDefaultChartConfig,
+};
+
+async function getDefaultChartConfig(chat_id: number, user_id: number, type: IChartType): Promise<ChartConfiguration> {
+    const chart_settings = await getChartSettings(type === "chat" ? chat_id : user_id, type);
+    const line_rgbValuesString = getRGBValueString(chart_settings.line_color);
+
+    return {
+        type: "line",
+        data: {
+            labels: [] as LabelItem[],
+            datasets: [
+                {
+                    // biome-ignore lint/suspicious/noExplicitAny: <lazyness>
+                    data: [] as any[],
+                    borderColor: `rgb(${line_rgbValuesString})`,
+                    borderCapStyle: "round",
+                    fill: true,
+                    backgroundColor: (context: any) => {
+                        if (!context.chart.chartArea) {
+                            return;
+                        }
+                        const { ctx, chartArea } = context.chart;
+                        const gradient = ctx.createLinearGradient(0, chartArea.top, 0, chartArea.bottom);
+                        gradient.addColorStop(1, `rgba(${line_rgbValuesString}, 0)`);
+                        gradient.addColorStop(0.6, `rgba(${line_rgbValuesString}, 0.4)`);
+                        gradient.addColorStop(0, `rgba(${line_rgbValuesString}, 0.9)`);
+                        return gradient;
+                    },
+                    tension: 0.2,
+                },
+            ],
+        },
+        options: {
+            layout: {
+                padding: {
+                    top: 80,
+                },
+            },
+            color: "#e8e7ec",
+            datasets: {
+                line: {
+                    pointRadius: 0,
+                },
+            },
+            plugins: {
+                legend: {
+                    display: false,
+                },
+            },
+            animation: false,
+            responsive: false,
+            scales: {
+                x: {
+                    grid: {
+                        display: false,
+                    },
+                    border: {
+                        display: false,
+                    },
+                    ticks: {
+                        color: `#${chart_settings.font_color}`,
+                        font: {
+                            weight: "bold",
+                        },
+                        textStrokeColor: "#000000",
+                        textStrokeWidth: 1,
+                    },
+                },
+                y: {
+                    min: type === "bot-all" ? 600000 : undefined,
+                    afterBuildTicks: (scale: Scale) => {
+                        if (type === "bot-all") {
+                            scale.ticks[0].value = 600000;
+                        }
+                    },
+                    grid: {
+                        display: false,
+                    },
+                    border: {
+                        display: false,
+                    },
+                    ticks: {
+                        color: `#${chart_settings.font_color}`,
+                        font: {
+                            weight: "bold",
+                        },
+                        textStrokeColor: "#000000",
+                        textStrokeWidth: 1,
+                    },
+                },
+            },
+        },
+        plugins: [await bgImagePlugin(chat_id, user_id, type)],
+    };
+}
+
+export { getChartConfig };
