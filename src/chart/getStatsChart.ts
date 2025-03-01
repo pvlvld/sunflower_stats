e3ec4b7773f113556ce3451d840d9e115a8f3a74
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-03-01 20:39:00 +0200

    refactor: move getRGBValueString function to a new hexToGgbString utility module

--- src/chart/getStatsChart.ts
+++ src/chart/getStatsChart.ts
@@ -443,9 +443,4 @@ async function destroyChart_Async(chart: Chart) {
     chart.destroy();
 }
 
-function getRGBValueString(hex: string) {
-    const rgb = hexToRGB(hex)!;
-    return `${rgb.r}, ${rgb.g}, ${rgb.b}`;
-}
-
 export { getStatsChartFromData, getChartTopChatsMonthly };
