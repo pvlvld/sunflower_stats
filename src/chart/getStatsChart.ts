0bda2b1be1a650c3b43254635a26cd55df1b172e
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-03-01 20:11:30 +0200

    fix: prevent drawing images for data points with y > 10 in getStatsChart

--- src/chart/getStatsChart.ts
+++ src/chart/getStatsChart.ts
@@ -261,17 +261,22 @@ function createProfileImagesPlugin(imageMap: Map<number, Image>) {
             const { ctx } = chart;
             const imageSize = 60;
 
+            let x = 0.0;
+            let y = 0.0;
+
             for (let datasetIndex = 0; datasetIndex < chart.data.datasets.length; datasetIndex++) {
-                const chat_id = chart.data.datasets[datasetIndex].chat_id;
+                const dataset = chart.data.datasets[datasetIndex];
+                const chat_id = dataset.chat_id;
                 const meta = chart.getDatasetMeta(datasetIndex);
 
                 if (!imageMap.has(chat_id)) continue;
 
                 const chatImage = imageMap.get(chat_id);
 
-                for (let point of meta.data) {
-                    const { x, y } = point.getCenterPoint();
-
+                for (let idx in meta.data) {
+                    // don't draw images for data points with y > 10
+                    if (dataset.data[idx].y === undefined || dataset.data[idx].y > 10) continue;
+                    ({ x, y } = meta.data[idx].getCenterPoint());
                     ctx.save();
                     ctx.beginPath();
                     ctx.arc(x, y, imageSize / 2, 0, Math.PI * 2, true);
