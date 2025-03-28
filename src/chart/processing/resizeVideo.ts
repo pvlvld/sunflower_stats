6c57c67735e88e01abadf3c6d2f26678442c759c
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-03-28 23:08:20 +0200

    init resizeVideo util

---
+++ src/chart/processing/resizeVideo.ts
@@ -0,0 +1,37 @@
+import Ffmpeg from "fluent-ffmpeg";
+import cfg from "../../config.js";
+import fs from "node:fs";
+import path from "node:path";
+
+function resizeVideo(id: number): Promise<void> {
+    return new Promise((resolve, reject) => {
+        const inputPath = path.join(cfg.PATHS.BASE_BG_PATH, `${id}.mp4`);
+        const tempPath = path.join(cfg.PATHS.BASE_BG_PATH, `${id}_resized.mp4`);
+        const outputPath = inputPath; // Final destination, gg
+
+        if (!fs.existsSync(inputPath)) {
+            return reject(new Error("File does not exist"));
+        }
+
+        const { width, height } = cfg.CHART;
+
+        Ffmpeg(inputPath)
+            .outputOptions([
+                `-vf scale=w=${width}:h=${height}:force_original_aspect_ratio=increase,crop=${width}:${height}`,
+                "-c:v libx264",
+                "-preset fast",
+                "-crf 23",
+                "-movflags +faststart",
+            ])
+            .on("end", () => {
+                fs.rename(tempPath, outputPath, (err) => {
+                    if (err) return reject(err);
+                    resolve();
+                });
+            })
+            .on("error", reject)
+            .save(tempPath);
+    });
+}
+
+export { resizeVideo };
