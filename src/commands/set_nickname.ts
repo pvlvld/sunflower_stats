a346c5680b351b256d5f7efd29c174169d719403
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-04-24 20:55:47 +0300

    set_nickname update to redis active

--- src/commands/set_nickname.ts
+++ src/commands/set_nickname.ts
@@ -1,30 +1,21 @@
 import type { IGroupTextContext } from "../types/context.js";
 import parseCmdArgs from "../utils/parseCmdArgs.js";
-import { active } from "../data/active.js";
 import Escape from "../utils/escape.js";
+import { active } from "../redis/active.js";
 
 async function set_nickname(ctx: IGroupTextContext) {
-    const chat_id = ctx.chat.id;
-    const user_id = ctx.from.id;
-    if (active.data[chat_id]![user_id] === undefined) {
-        return;
-    }
-
     const nickname = parseCmdArgs(ctx.msg.text as string).join(" ");
+
     if (nickname.length === 0) {
         await ctx.reply("+нік Х, +нікнейм Х — замість х бажаний нікнейм. Відображається замість імені у всіх командах");
-        return;
-    }
-
-    if (nickname.length > 20) {
+    } else if (nickname.length > 20) {
         await ctx.reply("Максимальна довжина нікнейму 20 символів.");
-        return;
+    } else {
+        active.updateUserField(ctx.chat.id, ctx.from.id, "nickname", Escape.html(nickname));
+        await ctx.reply(`✅ Успішно встановлено нікнейм: ${nickname}`, {
+            disable_notification: true,
+            link_preview_options: { is_disabled: true },
+        });
     }
-
-    active.data[chat_id]![user_id]!.nickname = Escape.html(nickname);
-    await ctx.reply(`✅ Успішно встановлено нікнейм: ${nickname}`, {
-        disable_notification: true,
-        link_preview_options: { is_disabled: true },
-    });
 }
 export default set_nickname;
