165c065d05ea83c53310a819166c595be530c9b7
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-04-25 13:54:29 +0300

    leaveChatMemberHandler update to redis active

--- src/handlers/leaveChatMember.ts
+++ src/handlers/leaveChatMember.ts
@@ -1,9 +1,9 @@
+import { active } from "../redis/active.js";
 import type { IContext } from "../types/context.js";
-import { active } from "../data/active.js";
 import type { Filter } from "grammy";
 
 function leaveChatMemberHandler(ctx: Filter<IContext, "chat_member">) {
-    delete active.data[ctx.chat.id]?.[ctx.chatMember.new_chat_member.user.id];
+    active.removeUser(ctx.chat.id, ctx.chatMember.new_chat_member.user.id);
 }
 
 export default leaveChatMemberHandler;
