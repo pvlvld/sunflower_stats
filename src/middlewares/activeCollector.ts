a877e96edc5660b207e6026e0ec6b28f46bd6d6e
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-04-29 22:26:47 +0300

    private messages fix, oopsie

--- src/middlewares/activeCollector.ts
+++ src/middlewares/activeCollector.ts
@@ -32,8 +32,8 @@ function ActiveCollectorWrapper() {
                 "",
                 ctx.from.username || ""
             );
-            return await next();
         }
+        return await next();
     };
 }
 
