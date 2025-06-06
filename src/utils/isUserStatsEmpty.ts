75c9a701ae9ddef34c4ae597fb6d331c3bc95c45
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-06-06 17:21:18 +0300

    cringe unneded opt

--- src/utils/isUserStatsEmpty.ts
+++ src/utils/isUserStatsEmpty.ts
@@ -1,7 +1,7 @@
 import type { IDBChatUserStatsPeriods } from "../types/stats.js";
 
 function isUserStatsEmpty(stats: IDBChatUserStatsPeriods): boolean {
-    return (Object.keys(stats) as (keyof IDBChatUserStatsPeriods)[]).every((k) => !stats[k]);
+    return stats.total === 0 && stats.year === 0 && stats.month === 0 && stats.week === 0 && stats.today === 0;
 }
 
 export { isUserStatsEmpty };
