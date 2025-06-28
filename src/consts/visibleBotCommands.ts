ab32cbbd0527abd5b9b91c94514e0fb94931ca73
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-06-28 23:55:05 +0300

    VISIBLE_BOT_COMMANDS

---
+++ src/consts/visibleBotCommands.ts
@@ -0,0 +1,36 @@
+const VISIBLE_BOT_COMMANDS = [
+    "help",
+    "donate",
+    "i",
+    "you",
+    "stats",
+    "statsall",
+    "settings",
+    "tchats",
+    "w",
+    "m",
+    "y",
+    "nick",
+    "setchatbg",
+    "setmybg",
+    "lang",
+];
+
+// ftl locale commands
+// bot-command-help = Команди та функції бота
+// bot-command-donate = Підтримати бота
+// bot-command-i = Моя статистика
+// bot-command-you = Статистика учасника
+// bot-command-stats = Статистика чату
+// bot-command-statsall = Статистика чату за весь час
+// bot-command-settings = Налаштування бота
+// bot-command-tchats = Рейтинг чатів
+// bot-command-w = Статистика за тиждень
+// bot-command-m = Статистика за місяць
+// bot-command-y = Статистика за рік
+// bot-command-nick = Змінити нік
+// bot-command-setchatbg = Змінити фон чату
+// bot-command-setmybg = Змінити мій фон
+// bot-command-lang = Змінити мову бота
+
+export { VISIBLE_BOT_COMMANDS };
