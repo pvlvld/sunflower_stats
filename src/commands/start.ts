7cf2559ffd5b0a62fdfc3db5e9dbdc6be7bcaafd
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-06-30 02:56:36 +0300

    make start_cmd async & return msg

--- src/commands/start.ts
+++ src/commands/start.ts
@@ -2,16 +2,18 @@ import cfg from "../config.js";
 import { IContext } from "../types/context.js";
 import start_menu from "../ui/menus/start.js";
 
-function start_cmd(ctx: IContext) {
-    ctx.replyWithPhoto(cfg.MEDIA.IMG.stats_example, {
-        caption: ctx.t("start"),
-        disable_notification: true,
-        reply_markup: start_menu,
-        reply_parameters: {
-            allow_sending_without_reply: true,
-            message_id: ctx.msg?.message_id ?? -1,
-        },
-    }).catch(console.error);
+async function start_cmd(ctx: IContext) {
+    return await ctx
+        .replyWithPhoto(cfg.MEDIA.IMG.stats_example, {
+            caption: ctx.t("start"),
+            disable_notification: true,
+            reply_markup: start_menu,
+            reply_parameters: {
+                allow_sending_without_reply: true,
+                message_id: ctx.msg?.message_id ?? -1,
+            },
+        })
+        .catch(console.error);
 }
 
 export { start_cmd };
