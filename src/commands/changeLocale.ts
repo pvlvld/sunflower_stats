14f1626d49b280612aec54342b029ea7f81adf19
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-06-29 23:57:33 +0300

    locale locale

--- src/commands/changeLocale.ts
+++ src/commands/changeLocale.ts
@@ -1,3 +1,4 @@
+import { LOCALE_LANGUAGE_MAP } from "../consts/localeLanguageMap.js";
 import type { ICommandContext } from "../types/context.js";
 import changeLocale_menu from "../ui/menus/changeLocaleMenu.js";
 import isChatOwner from "../utils/isChatOwner.js";
@@ -7,7 +8,11 @@ async function changeLocaleCommand(ctx: ICommandContext) {
     if (ctx.chat.type !== "private" && !(ctx.from && (await isChatOwner(ctx.chat.id, ctx.from.id)))) {
         return;
     }
-    const language = await localeNegotiator(ctx);
+
+    const localeCode = await localeNegotiator(ctx);
+    LOCALE_LANGUAGE_MAP;
+    const language = (<Record<string, string>>LOCALE_LANGUAGE_MAP)[localeCode] || localeCode;
+
     await ctx.reply(ctx.t("change-locale", { language }), {
         reply_markup: changeLocale_menu,
     });
