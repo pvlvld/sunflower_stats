78cbf7257199ff76976bb3f0a6d236b0319236f2
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-06-29 23:46:42 +0300

    locale change admins only

--- src/commands/changeLocale.ts
+++ src/commands/changeLocale.ts
@@ -1,8 +1,12 @@
 import type { ICommandContext } from "../types/context.js";
 import changeLocale_menu from "../ui/menus/changeLocaleMenu.js";
+import isChatOwner from "../utils/isChatOwner.js";
 import { localeNegotiator } from "../utils/localeNegotiator.js";
 
 async function changeLocaleCommand(ctx: ICommandContext) {
+    if (ctx.chat.type !== "private" && !(ctx.from && (await isChatOwner(ctx.chat.id, ctx.from.id)))) {
+        return;
+    }
     const language = await localeNegotiator(ctx);
     await ctx.reply(ctx.t("change-locale", { language }), {
         reply_markup: changeLocale_menu,
