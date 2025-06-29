2c5909578af62515fb122179cf9835a79e5dd1a3
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-06-29 23:04:59 +0300

    f

--- src/consts/localeLanguageMap.ts
+++ src/consts/localeLanguageMap.ts
@@ -1,18 +1,21 @@
 // Alphabetical order (almost)
 const LOCALE_LANGUAGE_MAP = Object.freeze({
-    uk: "Українська",
-    en: "English",
-    be: "Беларуская",
-    de: "Deutsch",
-    es: "Español",
-    fr: "Français",
-    hi: "हिंदी",
-    it: "Italiano",
-    ja: "日本語",
-    ko: "한국어",
-    pl: "Polski",
-    zh_CN: "中文 (简体)",
-    zh_HK: "中文 (繁體)",
+    uk: "Українська", //        Base
+    en: "English", //           Main
+    // be: "Беларуская",
+    // cs: "Čeština", //           Top priority
+    // de: "Deutsch", //           Top priority
+    // es: "Español", //           Top priority
+    // fr: "Français",
+    // hi: "हिंदी", //               Top priority
+    // id: "Bahasa Indonesia", //  Top priority
+    // it: "Italiano", //          Top priority
+    // ja: "日本語",
+    // ko: "한국어",
+    // pl: "Polski", //            Top priority
+    // pt: "Língua portuguesa", // Top priority
+    // zh_CN: "中文 (简体)",
+    // zh_HK: "中文 (繁體)",
 } as const);
 
 type ILocaleCode = keyof typeof LOCALE_LANGUAGE_MAP;
