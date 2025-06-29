92ff7dcfe24cad8d77c1c2d8d1a4b755c609d02d
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-06-30 02:41:15 +0300

    fix: admin fetch

--- src/utils/cacheChatAdmins.ts
+++ src/utils/cacheChatAdmins.ts
@@ -1,9 +1,16 @@
 import type { IChatAdmin } from "../cache/chatAdminsCache.js";
 import cacheManager from "../cache/cache.js";
 import bot from "../bot.js";
+import { GrammyError } from "grammy";
 
 async function cacheChatAdmins(chat_id: number) {
-    const apiAdmins = await bot.api.getChatAdministrators(chat_id).catch((e) => {
+    const apiAdmins = await bot.api.getChatAdministrators(chat_id).catch(async (e) => {
+        if (e instanceof GrammyError) {
+            if (e.parameters.migrate_to_chat_id) {
+                return await bot.api.getChatAdministrators(e.parameters.migrate_to_chat_id).catch((e) => {});
+            }
+        }
+
         console.error("Error caching chat admins.", e);
     });
     const admins: IChatAdmin[] = [];
