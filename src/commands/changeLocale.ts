aa42116ce66a4c782b833f3092ec6e8ccf449a18
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-06-30 23:45:00 +0300

    simplify required ctx type for changeLocaleCommand

--- src/commands/changeLocale.ts
+++ src/commands/changeLocale.ts
@@ -1,16 +1,18 @@
 import { LOCALE_LANGUAGE_MAP } from "../consts/localeLanguageMap.js";
-import type { ICommandContext } from "../types/context.js";
+import type { IContext } from "../types/context.js";
 import changeLocale_menu from "../ui/menus/changeLocaleMenu.js";
 import isChatOwner from "../utils/isChatOwner.js";
 import { localeNegotiator } from "../utils/localeNegotiator.js";
 
-async function changeLocaleCommand(ctx: ICommandContext, isForced = false) {
-    if (!isForced && ctx.chat.type !== "private" && !(ctx.from && (await isChatOwner(ctx.chat.id, ctx.from.id)))) {
-        return;
+async function changeLocaleCommand(ctx: IContext, isForced = false): Promise<void> {
+    if (!isForced) {
+        if (ctx.chat?.type !== "private" && !(ctx.from && ctx.chat && (await isChatOwner(ctx.chat.id, ctx.from.id)))) {
+            return;
+        }
     }
 
     const localeCode = await localeNegotiator(ctx);
-    const language = (<Record<string, string>>LOCALE_LANGUAGE_MAP)[localeCode] || localeCode;
+    const language = (LOCALE_LANGUAGE_MAP as Record<string, string>)[localeCode] || localeCode;
 
     await ctx.reply(ctx.t("change-locale", { language }), {
         reply_markup: changeLocale_menu,
