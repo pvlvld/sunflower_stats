875c46d98e03ce6c915eb7c3efe7dbb19059270a
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-07-03 20:11:14 +0300

    cache chat peak days stats

--- src/commands/peakDays.ts
+++ src/commands/peakDays.ts
@@ -1,23 +1,31 @@
+import cacheManager from "../cache/cache.js";
 import { Database } from "../db/db.js";
 import { IGroupHearsCommandContext } from "../types/context.js";
 
 async function peakDays(ctx: IGroupHearsCommandContext) {
-    const data = (
-        await Database.poolManager.getPool.query({
-            text: `SELECT sum(count) as count, TO_CHAR(date, 'YYYY-MM-DD') FROM stats_daily WHERE chat_id = ${ctx.chat.id} GROUP BY date ORDER BY count DESC LIMIT 25;`,
-            rowMode: "array",
-        })
-    ).rows;
+    let peakDays = cacheManager.TextCache.get(`peakDays:${ctx.chat.id}`) || "";
 
-    if (!data || data.length === 0) {
-        return void (await ctx.reply(ctx.t("error")));
+    if (!peakDays) {
+        console.log("cache miss");
+        const data = (
+            await Database.poolManager.getPool.query({
+                text: `SELECT sum(count) as count, TO_CHAR(date, 'YYYY-MM-DD') FROM stats_daily WHERE chat_id = ${ctx.chat.id} GROUP BY date ORDER BY count DESC LIMIT 25;`,
+                rowMode: "array",
+            })
+        ).rows;
+
+        if (!data || data.length === 0) {
+            return void (await ctx.reply(ctx.t("error")));
+        }
+
+        peakDays = data
+            .map((row) => {
+                return `${row[1]}: ${(<number>row[0]).toLocaleString("fr-FR")}`;
+            })
+            .join("\n");
+
+        cacheManager.TextCache.set(`peakDays:${ctx.chat.id}`, peakDays);
     }
-
-    const peakDays = data
-        .map((row) => {
-            return `${row[1]}: ${(<number>row[0]).toLocaleString("fr-FR")}`;
-        })
-        .join("\n");
 
     await ctx.reply(`${ctx.t("chat-peak-days")}\n\n<code>${peakDays}</code>`).catch((e) => {});
 }
