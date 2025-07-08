313d9b430948ba2c7d36b90a3fd1c1d024a815de
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-07-08 19:44:36 +0300

    don't send adv to the not uk locale chats

--- src/commands/staff/broadcast_chats.ts
+++ src/commands/staff/broadcast_chats.ts
@@ -34,6 +34,7 @@ async function broadcast_chats_cmd(ctx: IGroupHearsContext): Promise<void> {
     let chatMembersCount: number = 0;
     for (let chat of chats) {
         if (chat > 0) continue;
+        if ((await LocaleService.get(chat)) != "uk") return;
         users = await active.getChatUsers(chat);
         for (let user in users) {
             if (moment().diff(moment(users[user].active_last), "days") < 7) {
