3b2cd47d477286ef36aae23db1f8772380ed86bc
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-07-31 00:24:27 +0300

    chore

--- packages/chart_service/utils/isPremium.ts
+++ packages/chart_service/utils/isPremium.ts
@@ -2,4 +2,4 @@ import { IChartTask } from "@sunflower-stats/shared/index.js";
 
 export function isPremium(task: IChartTask, target_id: number): boolean {
     return target_id > 0 ? task.user_premium : task.chat_premium;
-} 
+}
