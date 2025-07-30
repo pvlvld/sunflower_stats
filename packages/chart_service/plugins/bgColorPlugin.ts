e2aa749dd9d97487b7975ab6de78c2a665494df8
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-07-30 23:55:36 +0300

    feat: add custom background color plugin for Chart.js

---
+++ packages/chart_service/plugins/bgColorPlugin.ts
@@ -0,0 +1,16 @@
+import { Chart } from "chart.js";
+
+const bgColorPlugin = {
+    id: "customCanvasBackgroundColor",
+    beforeDraw: (chart: Chart, args: unknown, options: { color?: string }) => {
+        const { ctx } = chart;
+        ctx.save();
+        ctx.globalCompositeOperation = "destination-over";
+        ctx.fillStyle = options.color || "#000000";
+        ctx.fillRect(0, 0, chart.width, chart.height);
+        ctx.restore();
+    },
+};
+
+export { bgColorPlugin };
+//     sqlResults.forEach((row) => {
