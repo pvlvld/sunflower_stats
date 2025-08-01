d1794dcc41924c983096cc17eea91e47ba59c6be
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-08-01 20:55:38 +0300

    feat: add BOT_TOKEN to config for bot authentication

--- packages/chart_service/consts/config.ts
+++ packages/chart_service/consts/config.ts
@@ -2,6 +2,8 @@ export const config = {
     CHART: { width: 1280, height: 640, ratio: 1280 / 640 },
     PATHS: {
         BASE_BG_PATH: "../data/chartBg",
+        BASE_AVATAR_PATH: "../data/profileImages",
     },
     BOT_ID: 6081112363,
+    BOT_TOKEN: -1,
 } as const;
