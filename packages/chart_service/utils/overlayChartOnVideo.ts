99f8a7755aab33316f6d768b545843c99c6e62b3
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-08-01 18:51:54 +0300

    feat: implement overlayChartOnVideo function for video processing with charts

---
+++ packages/chart_service/utils/overlayChartOnVideo.ts
@@ -0,0 +1,40 @@
+import Ffmpeg from "fluent-ffmpeg";
+import { Readable, Stream } from "stream";
+import { isPremium } from "./isPremium.js";
+import { config } from "consts/config.js";
+import { IChartTask } from "@sunflower-stats/shared/index.js";
+
+async function overlayChartOnVideo(chartBuffer: Buffer, task: IChartTask): Promise<Buffer> {
+    const imageStream = Readable.from(chartBuffer);
+    const outputChunks: Buffer[] = [];
+    let target_id = task.target_id;
+
+    if (isPremium(task, task.chat_id) && task.usechatbgforall) {
+        target_id = task.chat_id;
+    }
+
+    return new Promise((resolve, reject) => {
+        const passthroughStream = new Stream.PassThrough();
+        // const ffmpegTime = new Date().getTime();
+        Ffmpeg(`${config.PATHS.BASE_BG_PATH}/${target_id}.mp4`)
+            .input(imageStream)
+            .complexFilter([`overlay=W-w-0:H-h-0`])
+            .format("mp4")
+            .outputOptions(["-movflags frag_keyframe+empty_moov", "-preset superfast", "-crf 26"])
+            .on("error", reject)
+            // .on("stderr", console.error)
+            .pipe(passthroughStream);
+
+        passthroughStream.on("data", (buf) => outputChunks.push(buf));
+        passthroughStream.on("error", (err) => {
+            console.error("Error in passthrough stream:", err);
+            reject(err);
+        });
+        passthroughStream.on("end", () => {
+            resolve(Buffer.concat(outputChunks));
+            // console.log(`FFmpeg time: ${new Date().getTime() - ffmpegTime}ms`);
+        });
+    });
+}
+
+export { overlayChartOnVideo };
