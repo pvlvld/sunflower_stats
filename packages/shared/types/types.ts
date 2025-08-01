2f9710af1465590479278a2724d70099cfdb0d9a
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-08-01 18:34:24 +0300

    feat: add target_id to IChartTask interface for improved task identification

--- packages/shared/types/types.ts
+++ packages/shared/types/types.ts
@@ -1,5 +1,6 @@
 export interface IChartTask {
     taskId: string; // chat_id:user_id
+    target_id: number; // user_id or chat_id
     chat_id: number;
     user_id: number;
     reply_to_message_id: number;
