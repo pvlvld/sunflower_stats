a0a07ef29cacabde6781a943cfae29a683adce12
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-08-01 21:27:21 +0300

    refactor: update database query method to use DBPoolManager.getPool

--- packages/chart_service/utils/getChartData.ts
+++ packages/chart_service/utils/getChartData.ts
@@ -1,11 +1,11 @@
 import { IChartTask } from "@sunflower-stats/shared/index.js";
+import { DBPoolManager } from "db/db.js";
 
 // TODO: Database
-import { DBPoolManager } from "../../db/poolManager.js";
 
 async function getChatData(task: IChartTask) {
     return (
-        await DBPoolManager.getPoolRead.query(`
+        await DBPoolManager.getPool.query(`
       SELECT to_char(date, 'YYYY-MM-DD') AS x, SUM(count) AS y
           FROM stats_daily
           WHERE chat_id = ${task.chat_id} AND date BETWEEN '${task.date_from}' AND '${task.date_until}'
@@ -16,7 +16,7 @@ async function getChatData(task: IChartTask) {
 
 async function getUserData(task: IChartTask) {
     return (
-        await DBPoolManager.getPoolRead.query(
+        await DBPoolManager.getPool.query(
             `SELECT to_char(date, 'YYYY-MM-DD') AS x, count AS y
       FROM stats_daily
       WHERE user_id = ${task.user_id} AND chat_id = ${task.chat_id}
@@ -27,7 +27,7 @@ async function getUserData(task: IChartTask) {
 
 async function getBotData() {
     return (
-        await DBPoolManager.getPoolRead.query(
+        await DBPoolManager.getPool.query(
             `SELECT to_char(date, 'YYYY-MM-DD') AS x, SUM(count) AS y
                     FROM stats_daily
                     WHERE date > CURRENT_DATE - INTERVAL '1 year' 
