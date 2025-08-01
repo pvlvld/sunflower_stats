bf01f9b213501b6cd1e13a380a7c99c483d8b020
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-08-01 22:27:48 +0300

    feat: refactor config to load environment variables and define configuration structure

--- packages/chart_service/consts/config.ts
+++ packages/chart_service/consts/config.ts
@@ -1,9 +1,40 @@
-export const config = {
-    CHART: { width: 1280, height: 640, ratio: 1280 / 640 },
+import dotenv from "dotenv";
+dotenv.config();
+const requiredEnv = ["BOT_TOKEN", "DB_HOST", "DB_USER", "DB_PASSWORD", "DB_DATABASE"] as const;
+
+type ICfg = Record<(typeof requiredEnv)[number], string> & {
+    CHART: { width: number; height: number; ratio: number };
     PATHS: {
+        BASE_BG_PATH: string;
+        BASE_AVATAR_PATH: string;
+    };
+    BOT_ID: number;
+};
+
+function getConfig() {
+    const cfg: ICfg = {} as ICfg;
+    for (const e of requiredEnv) {
+        if (e in process.env) {
+            cfg[e] = process.env[e]!;
+            continue;
+        }
+        throw new Error(`Bruh, fix your .env! Where's the ${e}?`);
+    }
+
+    cfg.CHART = {
+        width: 1280,
+        height: 640,
+        ratio: 1280 / 640,
+    };
+
+    cfg.PATHS = {
         BASE_BG_PATH: "../data/chartBg",
         BASE_AVATAR_PATH: "../data/profileImages",
-    },
-    BOT_ID: 6081112363,
-    BOT_TOKEN: -1,
-} as const;
+    };
+
+    cfg.BOT_ID = 6081112363;
+
+    return cfg;
+}
+
+export const config = getConfig();
