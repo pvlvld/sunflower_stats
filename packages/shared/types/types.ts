bd8fd12655f21b99f1a2d815afb5c95b491e8bc5
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-08-01 18:49:45 +0300

    feat: add additional properties to IChartTask for enhanced chart customization

--- packages/shared/types/types.ts
+++ packages/shared/types/types.ts
@@ -9,6 +9,13 @@ export interface IChartTask {
     date_until: string;
     chat_premium: boolean;
     user_premium: boolean;
+
+    // TODO: Refactor out these settings to handle them on the bot side to minimize data transfer
+    usechatbgforall: boolean; // Use chat background for all charts
+    chat_line: string; // Color of the chat line
+    user_line: string; // Color of the user line
+    chat_font: string; // Font color for the chat
+    user_font: string; // Font color for the user
 }
 
 export interface IChartResult {
