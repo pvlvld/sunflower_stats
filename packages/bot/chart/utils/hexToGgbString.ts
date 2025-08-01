32b34a610e3e49007a9812d4e1b96217b0e870bb
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-08-01 19:52:50 +0300

    feat: move hexToRGB function and RGB class into hexToGgbString.ts

--- packages/bot/chart/utils/hexToGgbString.ts
+++ packages/bot/chart/utils/hexToGgbString.ts
@@ -1,4 +1,25 @@
-import { hexToRGB } from "../../utils/hexToRGB.js";
+function hexToRGB(hex: string | undefined) {
+    const length = hex?.length;
+    if (!length || length > 7 || length < 6) {
+        return undefined;
+    }
+
+    const offset = length === 7 ? 1 : 0;
+
+    const r = parseInt(hex.slice(0 + offset, 2 + offset), 16);
+    const g = parseInt(hex.slice(2 + offset, 4 + offset), 16);
+    const b = parseInt(hex.slice(4 + offset, 6 + offset), 16);
+
+    if ([r, g, b].some(isNaN)) {
+        return undefined;
+    }
+
+    return new RGB(r, g, b);
+}
+
+class RGB {
+    constructor(public r: number, public g: number, public b: number) {}
+}
 
 function hexToGgbString(hex: string) {
     const rgb = hexToRGB(hex)!;
