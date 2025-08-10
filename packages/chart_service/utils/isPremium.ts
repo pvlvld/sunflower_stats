b8cb6d5a0d593ba8346167483649d67016268e96
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-08-11 00:35:32 +0300

    refactor: correct type import for isPremium function to use IChartStatsTask

--- packages/chart_service/utils/isPremium.ts
+++ packages/chart_service/utils/isPremium.ts
@@ -1,5 +1,5 @@
-import type { IChartTask } from "@sunflower-stats/shared";
+import type { IChartStatsTask } from "@sunflower-stats/shared";
 
-export function isPremium(task: IChartTask, target_id: number): boolean {
+export function isPremium(task: IChartStatsTask, target_id: number): boolean {
     return target_id > 0 ? task.user_premium : task.chat_premium;
 }
