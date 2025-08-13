38c27b92e1dca6e9c700b4ef339936976312855f
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-08-14 00:48:24 +0300

    refactor: enhance IFormattedRangeDateGetters to include descriptive labels for date ranges

--- packages/bot/utils/date.ts
+++ packages/bot/utils/date.ts
@@ -1,40 +1,48 @@
 import moment from "moment";
 
 export interface IFormattedRangeDateGetters {
-    today: [string, string];
-    yesterday: [string, string];
-    weekRange: [monday: string, sunday: string];
-    monthRange: [firstDayOfTheMonth: string, lastDayOfTheMonth: string];
-    yearRange: [firstDayOfYear: string, lastDayOfYear: string];
-    all: [from: string, to: string];
+    today: [string, string, "today"];
+    yesterday: [string, string, "yesterday"];
+    weekRange: [string, string, "weekRange"];
+    monthRange: [string, string, "monthRange"];
+    yearRange: [string, string, "yearRange"];
+    all: [string, string, "all"];
 }
 
 /** Dates in "YYY-MM-DD" format.*/
 export class FormattedDate {
     get today(): IFormattedRangeDateGetters["today"] {
         const today = moment().format("YYYY-MM-DD");
-        return [today, today];
+        return [today, today, "today"];
     }
 
     get yesterday(): IFormattedRangeDateGetters["yesterday"] {
         const yesterday = moment().subtract(1, "days").format("YYYY-MM-DD");
-        return [yesterday, yesterday];
+        return [yesterday, yesterday, "yesterday"];
     }
 
     get weekRange(): IFormattedRangeDateGetters["weekRange"] {
-        return [moment().startOf("isoWeek").format("YYYY-MM-DD"), moment().endOf("isoWeek").format("YYYY-MM-DD")];
+        return [
+            moment().startOf("isoWeek").format("YYYY-MM-DD"),
+            moment().endOf("isoWeek").format("YYYY-MM-DD"),
+            "weekRange",
+        ];
     }
 
     get monthRange(): IFormattedRangeDateGetters["monthRange"] {
-        return [moment().startOf("month").format("YYYY-MM-DD"), moment().endOf("month").format("YYYY-MM-DD")];
+        return [
+            moment().startOf("month").format("YYYY-MM-DD"),
+            moment().endOf("month").format("YYYY-MM-DD"),
+            "monthRange",
+        ];
     }
 
     get yearRange(): IFormattedRangeDateGetters["yearRange"] {
-        return [moment().startOf("year").format("YYYY-MM-DD"), moment().endOf("year").format("YYYY-MM-DD")];
+        return [moment().startOf("year").format("YYYY-MM-DD"), moment().endOf("year").format("YYYY-MM-DD"), "yearRange"];
     }
 
     get all(): IFormattedRangeDateGetters["all"] {
-        return ["2013-08-14", moment().format("YYYY-MM-DD")];
+        return ["2013-08-14", moment().format("YYYY-MM-DD"), "all"];
     }
 
     public dateToYYYYMMDD(date: Date) {
