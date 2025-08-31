c8e854e0c9a76c9f4ae07b1f915d8de7f55c685f
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-08-31 18:04:06 +0300

    refactor: enhance date regex pattern for stats command to improve date format handling

--- packages/bot/commands/index.ts
+++ packages/bot/commands/index.ts
@@ -104,7 +104,7 @@ function regCommands() {
     // STATS
 
     groupStats.hears(
-        /^(!|\/)?(стата|статистика|stats)(@[a-zA-Z_]+)? \d{4}(\.|-)\d{2}(\.|-)\d{2}( \d{4}(\.|-)\d{2}(\.|-)\d{2})?$/i,
+        /^(!|\/)?(стата|статистика|stats)(@[a-zA-Z_]+)? ((\d{4}[-.]\d{2}[-.]\d{2})|(\d{2}[-.]\d{2}[-.]\d{4}))( ((\d{4}[-.]\d{2}[-.]\d{2})|(\d{2}[-.]\d{2}[-.]\d{4})))?$/i,
         async (ctx) => {
             botStatsManager.commandUse("стата дата");
             await StatsService.getInstance().chatStatsCallback(ctx);
